8fd2221d8de72c56b199625169e0a89be2c212e2
鄭雯宇 <zhengwenyu@android-24484aebc90de411.home>
2019-01-13 14:23:59 +0000

    started on find_even_index

---
+++ Kyu6/find_even_index.rb
@@ -0,0 +1,3 @@
+def find_even_index(arr)
+  #your code here
+end
--- Kyu7/all_inclusive.rb
+++ Kyu7/all_inclusive.rb
@@ -1,14 +1,3 @@
-# def contain_all_rots(string, arr)
-#   result = []
-#   for i in 0...string.length
-#     result << string[i..-1] + string[0...i]
-#   end
-#   result.each do |x|
-#     return false  if arr.include?(x) == false
-#   end
-#   return true
-# end
-
 def contain_all_rots(string, arr)
   result = []
   for i in 0...string.length
