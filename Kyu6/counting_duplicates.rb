3419a38a61d44baf3e9fa40aee783f31b9c0cd4a
Wenyu Zheng <you@example.com>
2019-02-01 12:52:41 +0000

    edited counting duplicates

--- Kyu6/counting_duplicates.rb
+++ Kyu6/counting_duplicates.rb
@@ -1,16 +1,11 @@
 def duplicate_count(text)
+  sum = 0
   new_text = text.downcase.split("")
-  result = []
-  new_text.each do |x|
-    result << new_text.count(x)
-  end
-  return result
-
-
-
+  result = new_text.map {|letter| new_text.count(letter) > 1 ? sum = sum + 1 : sum}
+  return result[-1]
 end
 
 
 
-
 p duplicate_count("aAbcd")
+p duplicate_count("Indivisibilities")
--- Kyu6/find_even_index.rb
+++ Kyu6/find_even_index.rb
@@ -1,4 +1,16 @@
 def find_even_index(arr)
-  #your code here
-  return 3
+  sum_on_left = 0
+  sum_on_right = 0
+
+  # for i in 0..arr.length-1
+  #   sum_on_left = sum_on_left + arr[i]
+  # end
+
+
+sum_on_left = sum_on_left + 
+
 end
+
+
+
+p find_even_index([1,2,3,4,3,2,1])
