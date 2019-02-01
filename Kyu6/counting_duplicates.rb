61cf4bdb2b11646463073fa4d6cbeef292c5e221
Wenyu Zheng <you@example.com>
2019-02-01 13:14:55 +0000

    pass counting duplicates

--- Kyu6/counting_duplicates.rb
+++ Kyu6/counting_duplicates.rb
@@ -1,11 +1,10 @@
 def duplicate_count(text)
-  sum = 0
   new_text = text.downcase.split("")
-  result = new_text.map {|letter| new_text.count(letter) > 1 ? sum = sum + 1 : sum}
-  return result[-1]
+  new_text.uniq.map.count {|letter| new_text.count(letter) > 1 }
 end
 
 
 
 p duplicate_count("aAbcd")
 p duplicate_count("Indivisibilities")
+p duplicate_count("abcdeaB")
