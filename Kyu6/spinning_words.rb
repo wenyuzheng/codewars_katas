41cdd9177f93c50332b3270098514f7b9ddc9029
Wenyu Zheng <you@example.com>
2019-02-05 23:18:50 +0000

    start words order

--- Kyu6/spinning_words.rb
+++ Kyu6/spinning_words.rb
@@ -1,8 +1,7 @@
 def spinWords(string)
-  string.split(" ").map{|words| words.length >= 5 ? words.reverse : words}
+  string.split(" ").map{|words| words.length >= 5 ? words.reverse : words}.join(" ")
 end
 
 
 
 p spinWords("Hey fellow warriors")
-# == "Hey wollef sroirraw"
---
+++ Kyu6/words_order.rb
@@ -0,0 +1,7 @@
+def order(words)
+  numbers = (1..9).to_a
+  words.split(" ").map
+end
+
+
+p order("is2 Thi1s T4est 3a")
