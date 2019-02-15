84b23e0568ce00e310d40cba57e70f4a2a9be55a
Wenyu Zheng <you@example.com>
2019-02-15 16:38:53 +0000

    edit dubstep

--- Kyu6/dubstep.rb
+++ Kyu6/dubstep.rb
@@ -1,9 +1,9 @@
 def song_decoder(song)
-  new_song = song.split("WUB").join
-  for i in 1...new_song.length-1
-    result = new_song.insert(i , " ")
-  end
-  return result
+  new_song = song.gsub("WUB","")
+  # for i in 1...new_song.length-1
+  #   result = new_song.insert(i , " ")
+  # end
+  # return result
 end
 
 
