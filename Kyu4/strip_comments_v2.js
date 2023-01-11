77db6160939166cb3df412f83fbd9f808c710f5a
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-11 16:44:02 +0000

    Five tests passed strip_comments

--- Kyu4/strip_comments_v2.js
+++ Kyu4/strip_comments_v2.js
@@ -5,9 +5,11 @@ function solution(input, markers) {
   // console.log({ inputLines });
 
   for (let i = 0; i < inputLines.length; i++) {
-    results.push(
-      inputLines[i].substring(0, inputLines[i].indexOf(markers[0])).trim()
-    );
+    let markerIndex = inputLines[i].indexOf(markers[0]);
+    if (markerIndex === -1) {
+      markerIndex = inputLines[i].length;
+    }
+    results.push(inputLines[i].substring(0, markerIndex).trim());
   }
 
   // console.log({ results });
@@ -15,7 +17,12 @@ function solution(input, markers) {
   return results.join("\n");
 }
 
-console.log([solution("test!delete me", ["!"])]); // test
-console.log([solution("test1!delete me\ntest2!delete me", ["!"])]); // test1\ntest2
-console.log([solution("tes!delete me\ntest2!delete me", ["!"])]); // tes\ntest2
-console.log([solution("tes   !delete me  \ntest2!delete me", ["!"])]); // tes\ntest2
+console.log(solution("test!delete me", ["!"]) === "test"); // test
+console.log(
+  solution("test1!delete me\ntest2!delete me", ["!"]) === "test1\ntest2"
+); // test1\ntest2
+console.log(solution("tes!delete me\ntest2!delete me", ["!"]) === "tes\ntest2"); // tes\ntest2
+console.log(
+  solution("tes   !delete me  \ntest2!delete me", ["!"]) === "tes\ntest2"
+); // tes\ntest2
+console.log(solution("tes     \ntest2!delete me", ["!"]) === "tes\ntest2"); // tes\ntest2
