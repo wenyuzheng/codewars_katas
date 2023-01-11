7b6c4bbba71eeb80c0ad45d892d73078de18d1eb
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-11 15:41:58 +0000

    Looking for the next non-letter char

--- Kyu4/strip_comments.js
+++ Kyu4/strip_comments.js
@@ -1,8 +1,23 @@
 function solution(input, markers) {
-  for (var marker of markers) {
+  console.log;
+  for (let i = markers.length - 1; i >= 0; i--) {
+    console.log({ i });
+    var marker = markers[i];
     console.log(marker);
-    console.log(input.indexOf(marker));
-    input.substring(input.indexOf(marker));
+
+    let end = input.indexOf(marker);
+
+    for (let j = input.indexOf(marker) + 1; j < input.length; j++) {
+      var c = input.charAt(j);
+      if (c.toLowerCase() != c.toUpperCase() && c != " ") {
+        console.log({ c });
+        let end = j;
+        console.log({ j });
+      }
+    }
+
+    input = input.substring(0, input.indexOf(marker));
+    console.log(input);
   }
   return input.trim();
 }
