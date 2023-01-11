23b504e6ac04c1feb0285c350da8baa14b15b48c
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-11 16:35:30 +0000

    3rd and 4th tests passed strip_comments

--- Kyu4/strip_comments_v2.js
+++ Kyu4/strip_comments_v2.js
@@ -2,12 +2,20 @@ function solution(input, markers) {
   const inputLines = input.split("\n");
   const results = [];
 
+  // console.log({ inputLines });
+
   for (let i = 0; i < inputLines.length; i++) {
-    results.push(inputLines[i].substring(0, input.indexOf(markers[0])));
+    results.push(
+      inputLines[i].substring(0, inputLines[i].indexOf(markers[0])).trim()
+    );
   }
+
+  // console.log({ results });
 
   return results.join("\n");
 }
 
 console.log([solution("test!delete me", ["!"])]); // test
 console.log([solution("test1!delete me\ntest2!delete me", ["!"])]); // test1\ntest2
+console.log([solution("tes!delete me\ntest2!delete me", ["!"])]); // tes\ntest2
+console.log([solution("tes   !delete me  \ntest2!delete me", ["!"])]); // tes\ntest2
