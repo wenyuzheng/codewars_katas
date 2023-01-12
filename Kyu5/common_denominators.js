d9bb76d7e9d577b69d05137006dd7a659a2eb9cb
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-12 22:07:16 +0000

    Pass all tests

--- Kyu5/common_denominators.js
+++ Kyu5/common_denominators.js
@@ -1,7 +1,17 @@
 function convertFrac(lst) {
-  const test = lst.map((e) => "(" + e[0] + "," + e[1] + ")");
-  console.log({ test });
-  return test.join("");
+  if (lst.length === 1) {
+    return "(" + lst[0][0] + "," + lst[0][1] + ")";
+  }
+  let denominator = lst[0][0];
+
+  for (let i = 0; i < lst.length - 1; i++) {
+    const next = lst[i + 1];
+    denominator = lcm(lst[i][1], next[1]);
+  }
+
+  return lst
+    .map((e) => "(" + (denominator / e[1]) * e[0] + "," + denominator + ")")
+    .join("");
 }
 
 function gcd(a, b) {
@@ -12,25 +22,39 @@ function lcm(a, b) {
   return (a * b) / gcd(a, b);
 }
 
-console.log(gcd(1, 2)); // 1
-console.log(gcd(3, 2)); // 1
-console.log(gcd(3, 6)); // 3
+// console.log(gcd(1, 2)); // 1
+// console.log(gcd(3, 2)); // 1
+// console.log(gcd(3, 6)); // 3
 
-console.log(lcm(3, 6)); // 6
-console.log(lcm(2, 3)); // 6
-console.log(lcm(6, 4)); // 12
+// console.log(lcm(3, 6)); // 6
+// console.log(lcm(2, 3)); // 6
+// console.log(lcm(6, 4)); // 12
 
-// console.log(convertFrac([[1, 2]]) === "(1,2)"); // (1,2)
-// console.log(
-//   convertFrac([
-//     [1, 2],
-//     [1, 3],
-//   ]) === "(1,2)(1,3)"
-// ); // test for format: (1,2)(1,3)
-// console.log(
-//   convertFrac([
-//     [1, 2],
-//     [1, 3],
-//   ])
-// ); // (3,6)(2,6)
-// console.log(convertFrac([[1, 2], [1, 3], [1, 4]])); // (6,12)(4,12)(3,12)
+console.log(convertFrac([[1, 2]]) === "(1,2)"); // (1,2)
+console.log(
+  convertFrac([
+    [1, 2],
+    [1, 3],
+  ]) === "(3,6)(2,6)"
+); // (3,6)(2,6)
+console.log(
+  convertFrac([
+    [1, 2],
+    [1, 3],
+    [1, 4],
+  ]) === "(6,12)(4,12)(3,12)"
+); // (6,12)(4,12)(3,12)
+console.log(
+  convertFrac([
+    [1, 12],
+    [1, 12],
+    [1, 12],
+  ]) === "(1,12)(1,12)(1,12)"
+); // (1,12)(1,12)(1,12)
+console.log(
+  convertFrac([
+    [2, 3],
+    [1, 3],
+    [1, 4],
+  ]) === "(8,12)(4,12)(3,12)"
+); // (8,12)(4,12)(3,12)
