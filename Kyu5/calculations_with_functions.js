227ec63630225bd58c2091950c6b8813eaa76c8f
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-13 20:28:40 +0000

    Refactor

--- Kyu5/calculations_with_functions.js
+++ Kyu5/calculations_with_functions.js
@@ -1,40 +1,46 @@
-function zero() {}
-function one() {}
-function two() {}
-function three() {}
-function four() {
-  return 4;
-}
-function five() {
-  return 5;
-}
-function six() {}
-function seven(str = "7") {
-  if (str[0] === "+") {
-    return 7 + Number(str[1]);
-  }
-  if (str[0] === "*") {
-    return 7 * Number(str[1]);
-  }
+const numberFuncGen = (n) => {
+  return (str) => {
+    if (str === undefined) {
+      return n;
+    }
+    if (str[0] === "+") {
+      return n + Number(str[1]);
+    }
+    if (str[0] === "*") {
+      return n * Number(str[1]);
+    }
+    if (str[0] === "-") {
+      return n - Number(str[1]);
+    }
+    if (str[0] === "/") {
+      return Math.floor(n / Number(str[1]));
+    }
+  };
+};
 
-  return 7;
-}
-function eight() {
-  return 8;
-}
-function nine() {}
+const zero = numberFuncGen(0);
+const one = numberFuncGen(1);
+const two = numberFuncGen(2);
+const three = numberFuncGen(3);
+const four = numberFuncGen(4);
+const five = numberFuncGen(5);
+const six = numberFuncGen(6);
+const seven = numberFuncGen(7);
+const eight = numberFuncGen(8);
+const nine = numberFuncGen(9);
 
-function plus(number) {
-  return "+" + number;
-}
-function minus() {}
-function times(number) {
-  return "*" + number;
-}
-function dividedBy() {}
+const operatorGen = (o) => (n) => o + n;
+
+const plus = operatorGen("+");
+const minus = operatorGen("-");
+const times = operatorGen("*");
+const dividedBy = operatorGen("/");
 
 console.log(seven(times(five()))); //35
 console.log(seven(times(four()))); //28
 console.log(seven(times(eight()))); //56
 console.log(seven(times(seven()))); //49
 console.log(seven(plus(five()))); //12
+console.log(seven(plus(seven()))); //14
+console.log(seven(dividedBy(five()))); //1
+console.log(seven(minus(seven()))); //0
