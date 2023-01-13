febd88b56e46cdc5992f133ac4eb7c1f4758c5c2
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-13 18:27:20 +0000

    common_denominators pass

--- Kyu5/common_denominators.js
+++ Kyu5/common_denominators.js
@@ -1,28 +1,7 @@
 function convertFrac(lst) {
-  if (lst.length === 1) {
-    return "(" + lst[0][0] + "," + lst[0][1] + ")";
-  }
-
-  lst = lst.map((e) => {
-    if (Math.floor(e[0]) !== e[0]) {
-      const decimalsNum = e[0].toString().split(".")[1].length;
-
-      return [e[0] * 10 * decimalsNum, e[1] * 10 * decimalsNum];
-    } else return e;
-  });
-
-  // Simplify
-  lst = lst.map((e) => {
-    const diff = gcd(e[0], e[1]);
-    return [e[0] / diff, e[1] / diff];
-  });
-
-  // Get common denominator
-  let denominator = lst[0][0];
-  for (let i = 0; i < lst.length - 1; i++) {
-    const next = lst[i + 1];
-    denominator = lcm(lst[i][1], next[1]);
-  }
+  const denominator = lst.reduce((a, b) => {
+    return lcm(a, b[1]);
+  }, 1);
 
   return lst
     .map((e) => "(" + (denominator / e[1]) * e[0] + "," + denominator + ")")
@@ -46,42 +25,41 @@ function lcm(a, b) {
 // console.log(lcm(2, 3)); // 6
 // console.log(lcm(6, 4)); // 12
 
-// console.log(convertFrac([[1, 2]]) === "(1,2)"); // (1,2)
-// console.log(
-//   convertFrac([
-//     [1, 2],
-//     [1, 3],
-//   ]) === "(3,6)(2,6)"
-// ); // (3,6)(2,6)
-// console.log(
-//   convertFrac([
-//     [1, 2],
-//     [1, 3],
-//     [1, 4],
-//   ]) === "(6,12)(4,12)(3,12)"
-// ); // (6,12)(4,12)(3,12)
-// console.log(
-//   convertFrac([
-//     [1, 12],
-//     [1, 12],
-//     [1, 12],
-//   ]) === "(1,12)(1,12)(1,12)"
-// ); // (1,12)(1,12)(1,12)
-// console.log(
-//   convertFrac([
-//     [2, 3],
-//     [1, 3],
-//     [1, 4],
-//   ]) === "(8,12)(4,12)(3,12)"
-// ); // (8,12)(4,12)(3,12)
-// console.log(
-//   convertFrac([
-//     [2, 4],
-//     [2, 6],
-//     [2, 8],
-//   ])
-// ); // (6,12)(4,12)(3,12)
-
+console.log(convertFrac([[1, 2]]) === "(1,2)"); // (1,2)
+console.log(
+  convertFrac([
+    [1, 2],
+    [1, 3],
+  ]) === "(3,6)(2,6)"
+); // (3,6)(2,6)
+console.log(
+  convertFrac([
+    [1, 2],
+    [1, 3],
+    [1, 4],
+  ]) === "(6,12)(4,12)(3,12)"
+); // (6,12)(4,12)(3,12)
+console.log(
+  convertFrac([
+    [1, 12],
+    [1, 12],
+    [1, 12],
+  ]) === "(1,12)(1,12)(1,12)"
+); // (1,12)(1,12)(1,12)
+console.log(
+  convertFrac([
+    [2, 3],
+    [1, 3],
+    [1, 4],
+  ]) === "(8,12)(4,12)(3,12)"
+); // (8,12)(4,12)(3,12)
+console.log(
+  convertFrac([
+    [2, 4],
+    [2, 6],
+    [2, 8],
+  ])
+); // (6,12)(4,12)(3,12)
 console.log(
   convertFrac([
     [1390.6153846153845, 2620],
@@ -89,3 +67,5 @@ console.log(
     [1965, 2620],
   ])
 ); // (18078,34060)(2262,34060)(25545,34060)
+
+// expected '(1390.6153846153845,2620)(174,2620)(1965,2620)' to equal '(18078,34060)(2262,34060)(25545,34060)'
