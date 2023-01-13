a91c833dae0e438cc0da5ec3e79f5305ffde6a06
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-13 19:55:52 +0000

    Four tests pass

--- Kyu5/calculations_with_functions.js
+++ Kyu5/calculations_with_functions.js
@@ -2,15 +2,19 @@ function zero() {}
 function one() {}
 function two() {}
 function three() {}
-function four() {}
+function four() {
+  return 4;
+}
 function five() {
   return 5;
 }
 function six() {}
-function seven(number) {
+function seven(number = 1) {
   return 7 * number;
 }
-function eight() {}
+function eight() {
+  return 8;
+}
 function nine() {}
 
 function plus() {}
@@ -21,3 +25,6 @@ function times(number) {
 function dividedBy() {}
 
 console.log(seven(times(five()))); //35
+console.log(seven(times(four()))); //28
+console.log(seven(times(eight()))); //56
+console.log(seven(times(seven()))); //49
