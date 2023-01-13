72f9669d0e04d94fb100a023b3e21a0d162d2f80
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-13 19:12:19 +0000

    pass all tests

--- Kyu5/productFib.js
+++ Kyu5/productFib.js
@@ -1,5 +1,16 @@
 function productFib(prod) {
-  // ...
+  // Fib(n) * Fib(n+1)
+
+  let n = 0;
+
+  while (Fib(n) * Fib(n + 1) <= prod) {
+    if (Fib(n) * Fib(n + 1) === prod) {
+      return [Fib(n), Fib(n + 1), true];
+    }
+    n++;
+  }
+
+  return [Fib(n), Fib(n + 1), false];
 }
 
 function Fib(n) {
@@ -8,7 +19,9 @@ function Fib(n) {
   return Fib(n - 1) + Fib(n - 2);
 }
 
-console.log(Fib(2) === 1);
-console.log(Fib(3) === 2);
-console.log(Fib(8) === 21);
-console.log(Fib(10) === 55);
+// console.log(Fib(2) === 1);
+// console.log(Fib(3) === 2);
+// console.log(Fib(8) === 21);
+// console.log(Fib(10) === 55);
+
+console.log(productFib(0)); // [0,1,true]
