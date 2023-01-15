636c3be1a1ecc43264ea92bfbaa9d41214dc7706
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-15 18:17:07 +0000

    Start moving_zeros

---
+++ Kyu5/moving_zeros_to_end.js
@@ -0,0 +1,10 @@
+function moveZeros(arr) {
+  if (arr.length === 0) {
+    return arr;
+  }
+  return arr;
+}
+
+console.log(moveZeros([1])); // [1]
+console.log(moveZeros([0])); // [0]
+// console.log(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1])); // [1, 2, 1, 1, 3, 1, 0, 0, 0, 0]
