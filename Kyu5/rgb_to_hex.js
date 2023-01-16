a3a0161885e39d488a2d22831998a4d43c3ba9e2
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-16 01:18:12 +0000

    Refactor rgb conversion

--- Kyu5/rgb_to_hex.js
+++ Kyu5/rgb_to_hex.js
@@ -5,8 +5,13 @@ function rgb(r, g, b) {
 function convertToHex(val) {
   if (val < 0) val = 0;
   if (val > 255) val = 255;
-  const hex = val.toString(16).toUpperCase();
-  return hex.length === 1 ? "0" + hex : hex;
+
+  //   const hex = val.toString(16).toUpperCase();
+  //   return hex.length === 1 ? "0" + hex : hex;
+
+  // slice(-2) means get the last two elements
+  // e.g. str = "hello", str.slice(-2) = "lo"
+  return ("0" + val.toString(16)).slice(-2).toUpperCase();
 }
 
 console.log(rgb(0, 0, 0)); // returns 000000
