49f52f156406d618150ddd2966a1c5239cf21a9e
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-16 00:29:29 +0000

    Improve moveZeros

--- Kyu5/moving_zeros_to_end.js
+++ Kyu5/moving_zeros_to_end.js
@@ -1,21 +1,15 @@
 function moveZeros(arr) {
-  let countZero = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === 0) {
-      const zero = arr.splice(i, 1);
-      i--;
-      countZero++;
-    }
+  const filtered = arr.filter((e) => e !== 0);
+  const zeros = arr.length - filtered.length;
+  for (let i = 0; i < zeros; i++) {
+    filtered.push(0);
   }
-  for (let i = 0; i < countZero; i++) {
-    arr.push(0);
-  }
-  return arr;
+  return filtered;
 }
 
 // console.log(moveZeros([1])); // [1]
 // console.log(moveZeros([0])); // [0]
-console.log(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1])); // [1, 2, 1, 1, 3, 1, 0, 0, 0, 0]
+// console.log(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1])); // [1, 2, 1, 1, 3, 1, 0, 0, 0, 0]
 console.log(
   moveZeros([
     9, +0, 9, 1, 2, 1, 1, 3, 1, 9, +0, +0, 9, +0, +0, +0, +0, +0, +0, +0,
