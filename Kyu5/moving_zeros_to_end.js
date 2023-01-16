6bef79a83db151c5115c5e3dfbd563b9b3868287
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-16 00:05:50 +0000

    Pass first set of tests

--- Kyu5/moving_zeros_to_end.js
+++ Kyu5/moving_zeros_to_end.js
@@ -1,10 +1,17 @@
 function moveZeros(arr) {
-  if (arr.length === 0) {
-    return arr;
+  let countZero = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      arr.splice(i, 1);
+      countZero++;
+    }
+  }
+  for (let i = 0; i < countZero; i++) {
+    arr.push(0);
   }
   return arr;
 }
 
 console.log(moveZeros([1])); // [1]
 console.log(moveZeros([0])); // [0]
-// console.log(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1])); // [1, 2, 1, 1, 3, 1, 0, 0, 0, 0]
+console.log(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1])); // [1, 2, 1, 1, 3, 1, 0, 0, 0, 0]
