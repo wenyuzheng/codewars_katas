12bb1004cc6ffe88b8cea6b07325ffb8a5f7958e
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-16 01:45:09 +0000

    Pass three tests

--- Kyu5/directions_reduction.js
+++ Kyu5/directions_reduction.js
@@ -9,26 +9,18 @@ function dirReduc(arr) {
   const stack = [];
   while (arr.length !== 0) {
     const pop = arr.splice(0, 1)[0];
-    console.log(directions[pop]);
-
-    console.log(stack[0]);
     if (directions[pop] === stack[0]) {
-      console.log(" match");
       stack.splice(0, 1);
     } else {
-      console.log("no match");
-
       stack.push(pop);
     }
-    console.log({ stack });
-    console.log({ arr });
   }
 
   return stack;
 }
 console.log(dirReduc(["NORTH", "SOUTH"])); // []
-// console.log(
-//   dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"])
-// ); // ["WEST"]
+console.log(
+  dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"])
+); // ["WEST"]
+console.log(dirReduc(["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"])); // []
 // console.log(dirReduc(["NORTH", "WEST", "SOUTH", "EAST"])); // ["NORTH", "WEST", "SOUTH", "EAST"]
-// console.log(dirReduc(["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"])); // []
