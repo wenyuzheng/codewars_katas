97c20d0c85cfe9f39e83cc5edc047824b636a304
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-17 00:26:17 +0000

    Pass all tests

--- Kyu5/rot13.js
+++ Kyu5/rot13.js
@@ -29,18 +29,26 @@ function rot13(message) {
   ];
 
   return message
-    .split("")
-    .map((letter) => {
-      const index = alphabet.indexOf(letter.toUpperCase());
-      const rotIndex = index < 13 ? index + 13 : index - 13;
-      const rotLetter = alphabet[rotIndex];
-      return letter === letter.toLowerCase()
-        ? rotLetter.toLowerCase()
-        : rotLetter;
-    })
-    .join("");
+    .split(" ")
+    .map((word) =>
+      word
+        .split("")
+        .map((letter) => {
+          const index = alphabet.indexOf(letter.toUpperCase());
+          if (index === -1) return letter;
+          const rotIndex = index < 13 ? index + 13 : index - 13;
+          const rotLetter = alphabet[rotIndex];
+          return letter === letter.toLowerCase()
+            ? rotLetter.toLowerCase()
+            : rotLetter;
+        })
+        .join("")
+    )
+    .join(" ");
 }
 
-console.log(rot13("a")); //n
-console.log(rot13("test")); //grfg
-console.log(rot13("Test")); //Grfg
+// console.log(rot13("a")); //n
+// console.log(rot13("test")); //grfg
+// console.log(rot13("Test")); //Grfg
+console.log(rot13("Ruby is cool!")); // Ehol vf pbby!
+console.log(rot13("k#/^~6m.N,.K{2Y")); // x#/^~6z.A,.X{2L
