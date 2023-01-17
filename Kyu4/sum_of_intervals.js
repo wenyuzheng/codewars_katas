e09fa7d4240c24cc6c5f771f6e47c3d774d2607b
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-17 01:15:15 +0000

    New method not working

--- Kyu4/sum_of_intervals.js
+++ Kyu4/sum_of_intervals.js
@@ -1,7 +1,17 @@
 function sumIntervals(intervals) {
-  let sum = 0;
-  intervals.map((pair) => (sum += pair[1] - pair[0]));
-  return sum;
+  //   let sum = 0;
+  //   intervals.map((pair) => {
+  //     sum += pair[1] - pair[0];
+  //   });
+  //   return sum;
+
+  let min = intervals[0][0];
+  let max = intervals[0][1];
+  intervals.map((pair) => {
+    if (pair[0] < min) min = pair[0];
+    if (pair[1] > max) max = pair[1];
+  });
+  return max - min;
 }
 
 console.log(sumIntervals([[1, 5]])); // 4
@@ -11,10 +21,16 @@ console.log(
     [6, 10],
   ])
 ); // 8
-// console.log(
-//   sumIntervals([
-//     [1, 2],
-//     [6, 10],
-//     [11, 15],
-//   ])
-// ); // 9
+console.log(
+  sumIntervals([
+    [1, 2],
+    [6, 10],
+    [11, 15],
+  ])
+); // 9
+console.log(
+  sumIntervals([
+    [1, 4],
+    [3, 5],
+  ])
+); // 4
