d5741e79920cbca12efee76ebe5d8d0d8df64701
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-17 17:10:09 +0000

    Pass four tests

--- Kyu4/sum_of_intervals.js
+++ Kyu4/sum_of_intervals.js
@@ -1,17 +1,29 @@
 function sumIntervals(intervals) {
-  //   let sum = 0;
-  //   intervals.map((pair) => {
-  //     sum += pair[1] - pair[0];
-  //   });
-  //   return sum;
-
-  let min = intervals[0][0];
-  let max = intervals[0][1];
+  let sum = 0;
   intervals.map((pair) => {
-    if (pair[0] < min) min = pair[0];
-    if (pair[1] > max) max = pair[1];
+    let start = pair[0];
+    let end = pair[1];
+    for (let i = 0; i < intervals.length; i++) {
+      if (intervals[i] === pair) break;
+      if (intervals[i][0] < start && start < intervals[i][1]) {
+        start = intervals[i][1];
+      }
+      //   if (intervals[i][0]  start < intervals[i][1]) {
+      //     start = intervals[i][1];
+      //     console.log({ start });
+      //   }
+    }
+    sum += end - start;
   });
-  return max - min;
+  return sum;
+
+  //   let min = intervals[0][0];
+  //   let max = intervals[0][1];
+  //   intervals.map((pair) => {
+  //     if (pair[0] < min) min = pair[0];
+  //     if (pair[1] > max) max = pair[1];
+  //   });
+  //   return max - min;
 }
 
 console.log(sumIntervals([[1, 5]])); // 4
