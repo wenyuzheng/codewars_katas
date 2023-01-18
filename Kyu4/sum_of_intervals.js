b04a0663b2dc6d50fc016b4eeff9287c55ca0023
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-18 16:12:00 +0000

    New method pass tests

--- Kyu4/sum_of_intervals.js
+++ Kyu4/sum_of_intervals.js
@@ -1,10 +1,16 @@
 function sumIntervals(intervals) {
   let sum = 0;
-  intervals.map((pair) => {
+  intervals.forEach((pair) => {
     let start = pair[0];
     let end = pair[1];
     for (let i = 0; i < intervals.length; i++) {
-      if (intervals[i] === pair) break;
+      //   if (intervals[i][0] === start && intervals[i][1] === end) {
+      //     console.log({ pair });
+      //     break;
+      //   }
+      if (intervals[i] === pair) {
+        break;
+      }
       if (intervals[i][0] < start && end < intervals[i][1]) {
         start = 0;
         end = 0;
@@ -15,67 +21,94 @@ function sumIntervals(intervals) {
       if (intervals[i][0] <= end && end < intervals[i][1]) {
         end = intervals[i][0];
       }
+      // existing intervals is 2,5; new pair = 1,7
+      if (start <= intervals[i][0] && intervals[i][1] <= end) {
+        sum += intervals[i][0] - start + (intervals[i][1] - end);
+        start = 0;
+        end = 0;
+      }
     }
-
-    // const array = [];
-    // for (let j = start; j < end; j++) {
-    //   array.push(j);
-    // }
 
     sum += end >= 0 ? end - start : Math.abs(start - end);
   });
   return sum;
 }
 
-console.log(sumIntervals([[1, 5]])); // 4
+// console.log(sumIntervals([[1, 5]])); // 4
+// console.log(
+//   sumIntervals([
+//     [1, 5],
+//     [6, 10],
+//   ])
+// ); // 8
+// console.log(
+//   sumIntervals([
+//     [1, 2],
+//     [6, 10],
+//     [11, 15],
+//   ])
+// ); // 9
+// console.log(
+//   sumIntervals([
+//     [1, 4],
+//     [3, 5],
+//   ])
+// ); // 4
+// console.log(
+//   sumIntervals([
+//     [1, 4],
+//     [7, 10],
+//     [3, 5],
+//   ])
+// ); // 7
+// console.log(
+//   sumIntervals([
+//     [1, 5],
+//     [10, 20],
+//     [1, 6],
+//     [16, 19],
+//     [5, 11],
+//   ])
+// ); // 19
+// console.log(
+//   sumIntervals([
+//     [0, 20],
+//     [-100000000, 10],
+//     [30, 40],
+//   ])
+// ); // 100000030
+// console.log(
+//   sumIntervals([
+//     [-8, -5],
+//     [10, 19],
+//     [9, 18],
+//     [1, 9],
+//     [-14, -8],
+//   ])
+// ); // 27
+
+// console.log(
+//   sumIntervals([
+//     [-10, -6],
+//     [9, 16],
+//     [10, 18],
+//     [-1, 4],
+//     [13, 22],
+//     [-17, -8],
+//     [-3, 7],
+//     [-1, 6],
+//   ])
+// ); // 34
+
 console.log(
   sumIntervals([
-    [1, 5],
-    [6, 10],
+    [-10, -6],
+    [9, 16],
+    [10, 18],
+    [-1, 4],
+    [13, 22],
+    [-17, -8], // 29
+    [-3, 7],
+    // [-1, 6],
   ])
-); // 8
-console.log(
-  sumIntervals([
-    [1, 2],
-    [6, 10],
-    [11, 15],
-  ])
-); // 9
-console.log(
-  sumIntervals([
-    [1, 4],
-    [3, 5],
-  ])
-); // 4
-console.log(
-  sumIntervals([
-    [1, 4],
-    [7, 10],
-    [3, 5],
-  ])
-); // 7
-console.log(
-  sumIntervals([
-    [1, 5],
-    [10, 20],
-    [1, 6],
-    [16, 19],
-    [5, 11],
-  ])
-); // 19
-console.log(
-  sumIntervals([
-    [0, 20],
-    [-100000000, 10],
-    [30, 40],
-  ])
-); // 100000030
-console.log(
-  sumIntervals([
-    [-8, -5],
-    [10, 19],
-    [9, 18],
-    [1, 9],
-    [-14, -8],
-  ])
-); // 27
+); // 34
