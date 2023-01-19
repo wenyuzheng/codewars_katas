c5775d416b7226489c79678ab333ea2d6d7268a5
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-19 00:02:48 +0000

    Tested edge cases

--- Kyu4/sum_of_intervals_v2.js
+++ Kyu4/sum_of_intervals_v2.js
@@ -1,24 +1,38 @@
 function sumIntervals(intervals) {}
 
-function addOneInterval(existing, newIn) {
-  if (existing.length === 0) {
-    return [newIn];
+function addIntervals(currIntervals, newIntervals) {
+  if (newIntervals.length == 0) {
+    return currIntervals;
+  } else if (currIntervals.length == 0) {
+    currIntervals.push(newIntervals.shift());
+    addIntervals(currIntervals, newIntervals);
+  } else if (newIntervals.shift()) {
   }
-  let add = newIn;
-  existing.forEach((e) => {
-    if (e[0] <= newIn[0] && newIn[0] <= e[1] && e[1] <= newIn[1]) {
-      add = [e[1], newIn[1]];
-    } else if (newIn[0] < e[0] && e[0] <= newIn[1]) {
-      add = [newIn[0], e[0]];
-    } else if (e[0] <= newIn[0] && newIn[1] <= e[1]) {
-      add = [0, 0];
-    }
-  });
-  existing.push(add);
-  existing.sort((a, b) => a[0] - b[0]);
-
-  return existing;
+  return currIntervals;
 }
+
+console.log(addIntervals([10, 11], []));
+console.log(addIntervals([], [[-10, -6]]));
+
+// function addOneInterval(existing, newIn) {
+//   if (existing.length === 0) {
+//     return [newIn];
+//   }
+//   let add = newIn;
+//   existing.forEach((e) => {
+//     if (e[0] <= newIn[0] && newIn[0] <= e[1] && e[1] <= newIn[1]) {
+//       add = [e[1], newIn[1]];
+//     } else if (newIn[0] < e[0] && e[0] <= newIn[1]) {
+//       add = [newIn[0], e[0]];
+//     } else if (e[0] <= newIn[0] && newIn[1] <= e[1]) {
+//       add = [0, 0];
+//     }
+//   });
+//   existing.push(add);
+//   existing.sort((a, b) => a[0] - b[0]);
+
+//   return existing;
+// }
 
 // [
 //   [-10, -6], //->  [[-10,-6]]
@@ -31,71 +45,71 @@ function addOneInterval(existing, newIn) {
 //   [-1, 6], //-> [[-17,-6],[-1, 7],[9,22]]
 // ];
 
-console.log(addOneInterval([], [-10, -6])); // [[-10, -6]]
-console.log(
-  addOneInterval(
-    [
-      [1, 2],
-      [6, 7],
-      [10, 11],
-    ],
-    [8, 9]
-  )
-); // [[-10,-6],[9,16]]
-console.log(
-  addOneInterval(
-    [
-      [1, 2],
-      [6, 7],
-      [10, 11],
-    ],
-    [6.5, 9]
-  )
-); // [[1, 2],[6, 9],[10, 11]]
-// [[1, 2],[6, 7],[7,9],[10, 11]]
+// console.log(addOneInterval([], [-10, -6])); // [[-10, -6]]
+// console.log(
+//   addOneInterval(
+//     [
+//       [1, 2],
+//       [6, 7],
+//       [10, 11],
+//     ],
+//     [8, 9]
+//   )
+// ); // [[-10,-6],[9,16]]
+// console.log(
+//   addOneInterval(
+//     [
+//       [1, 2],
+//       [6, 7],
+//       [10, 11],
+//     ],
+//     [6.5, 9]
+//   )
+// ); // [[1, 2],[6, 9],[10, 11]]
+// // [[1, 2],[6, 7],[7,9],[10, 11]]
 
-console.log(
-  addOneInterval(
-    [
-      [1, 2],
-      [6, 7],
-      [10, 11],
-    ],
-    [8, 10.5]
-  )
-); // [[1, 2],[6, 7],[8, 10],[10, 11]]
+// console.log(
+//   addOneInterval(
+//     [
+//       [1, 2],
+//       [6, 7],
+//       [10, 11],
+//     ],
+//     [8, 10.5]
+//   )
+// ); // [[1, 2],[6, 7],[8, 10],[10, 11]]
 
-console.log(
-  addOneInterval(
-    [
-      [1, 2],
-      [6, 7],
-      [10, 11],
-    ],
-    [6.5, 6.6]
-  )
-); // [[1, 2],[6, 7],[10, 11]]
+// console.log(
+//   addOneInterval(
+//     [
+//       [1, 2],
+//       [6, 7],
+//       [10, 11],
+//     ],
+//     [6.5, 6.6]
+//   )
+// ); // [[1, 2],[6, 7],[10, 11]]
 
-console.log(
-  addOneInterval(
-    [
-      [1, 2],
-      [6, 7],
-      [10, 11],
-    ],
-    [5.5, 7.5]
-  )
-); // [[1, 2],[5.5, 6],[6, 7],[10, 11]]
-console.log(
-  addOneInterval(
-    [
-      [1, 2],
-      [6, 7],
-      [10, 11],
-    ],
-    [1.5, 8.5]
-  )
-); // [[1, 2],[5.5, 6],[6, 7],[10, 11]]
+// console.log(
+//   addOneInterval(
+//     [
+//       [1, 2],
+//       [6, 7],
+//       [10, 11],
+//     ],
+//     [5.5, 7.5]
+//   )
+// ); // [[1, 2],[5.5, 6],[6, 7],[10, 11]]
+// console.log(
+//   addOneInterval(
+//     [
+//       [1, 2],
+//       [6, 7],
+//       [10, 11],
+//     ],
+//     [1.5, 8.5]
+//   )
+// ); // [[1, 2],[],[6, 7],[[10, 11]]
 // console.log(
 //   addOneInterval(
 //     [
