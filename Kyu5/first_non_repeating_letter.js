3c0643e371127aeb1a328eed4876648b22cdc05e
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-20 00:50:50 +0000

    Challenge version of first non repeating letters

--- Kyu5/first_non_repeating_letter.js
+++ Kyu5/first_non_repeating_letter.js
@@ -1,13 +1,16 @@
 function firstNonRepeatingLetter(s) {
   let letters = {};
   s.split("").forEach((e) => {
-    letters.hasOwnProperty(e) ? (letters[e] += 1) : (letters[e] = 1);
+    const letter = e.toLowerCase();
+    return letters.hasOwnProperty(letter)
+      ? (letters[letter] += 1)
+      : (letters[letter] = 1);
   });
 
   let firstLetter = "";
   for (const key in letters) {
     if (letters[key] === 1) {
-      firstLetter = key;
+      firstLetter = s[s.toLowerCase().indexOf(key)];
       break;
     }
   }
@@ -17,3 +20,4 @@ function firstNonRepeatingLetter(s) {
 
 console.log(firstNonRepeatingLetter("a")); //"a"
 console.log(firstNonRepeatingLetter("stress")); //"t"
+console.log(firstNonRepeatingLetter("sTreSS")); //"T"
