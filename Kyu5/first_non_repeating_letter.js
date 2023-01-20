b3dc64c4f509ab94bcc96d2ba3f2ebca78e21266
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-20 00:55:04 +0000

    Improve method

--- Kyu5/first_non_repeating_letter.js
+++ Kyu5/first_non_repeating_letter.js
@@ -1,21 +1,31 @@
-function firstNonRepeatingLetter(s) {
-  let letters = {};
-  s.split("").forEach((e) => {
-    const letter = e.toLowerCase();
-    return letters.hasOwnProperty(letter)
-      ? (letters[letter] += 1)
-      : (letters[letter] = 1);
-  });
+// function firstNonRepeatingLetter(s) {
+//   let letters = {};
+//   s.split("").forEach((e) => {
+//     const letter = e.toLowerCase();
+//     return letters.hasOwnProperty(letter)
+//       ? (letters[letter] += 1)
+//       : (letters[letter] = 1);
+//   });
 
-  let firstLetter = "";
-  for (const key in letters) {
-    if (letters[key] === 1) {
-      firstLetter = s[s.toLowerCase().indexOf(key)];
-      break;
+//   let firstLetter = "";
+//   for (const key in letters) {
+//     if (letters[key] === 1) {
+//       firstLetter = s[s.toLowerCase().indexOf(key)];
+//       break;
+//     }
+//   }
+
+//   return firstLetter;
+// }
+
+function firstNonRepeatingLetter(s) {
+  let str = s.toLowerCase();
+  for (let i = 0; i < str.length; i++) {
+    if (str.indexOf(str[i]) === str.lastIndexOf(str[i])) {
+      return s[i];
     }
   }
-
-  return firstLetter;
+  return "";
 }
 
 console.log(firstNonRepeatingLetter("a")); //"a"
