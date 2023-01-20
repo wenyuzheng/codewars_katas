824a47563f47336b35535f1e7bce7a7c214de95c
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-20 21:52:01 +0000

    Pass tests

--- Kyu5/weight_for_weight.js
+++ Kyu5/weight_for_weight.js
@@ -1,15 +1,21 @@
 function orderWeight(string) {
-  let weights = {};
+  if (string.length <= 1) return string;
+  let weights = [];
   string
     .trim()
     .split(" ")
     .forEach((e) => {
       const w = e.split("").reduce((a, b) => Number(a) + Number(b), 0);
-      weights[e] = w;
+      weights.push([e, w]);
     });
 
-  return string;
+  const str = weights
+    .sort((a, b) => a[1] - b[1])
+    .map((e) => e[0].toString())
+    .join(" ");
+
+  return str;
 }
 
-console.log(orderWeight("")); // ""
+console.log(orderWeight("") === ""); // ""
 console.log(orderWeight("56 65 74 100 99 68 86 180 90")); // "100 180 90 56 65 74 68 86 99"
