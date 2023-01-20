76f3b937e49612ad23a7f9688d89087f9fa3b507
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-20 22:11:46 +0000

    increment string test case 1

---
+++ Kyu5/string_incrementer.js
@@ -0,0 +1,7 @@
+function incrementString(strng) {
+  return parseInt(strng) + 1;
+}
+
+console.log(incrementString("1")); // "2"
+// console.log(incrementString("009")); // "010"
+// console.log(incrementString("foo")); // "foo1"
