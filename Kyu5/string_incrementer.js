617bcd1729c0b456e075efa2c11c3f0b9f85ab79
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-20 22:23:44 +0000

    Pass two tests

--- Kyu5/string_incrementer.js
+++ Kyu5/string_incrementer.js
@@ -1,7 +1,12 @@
 function incrementString(strng) {
-  return parseInt(strng) + 1;
+  let incrementedStr = (parseInt(strng) + 1).toString();
+  if (incrementedStr.length < strng.length) {
+    incrementedStr = incrementedStr.padStart(strng.length, "0");
+  }
+
+  return incrementedStr;
 }
 
 console.log(incrementString("1")); // "2"
-// console.log(incrementString("009")); // "010"
+console.log(incrementString("009")); // "010"
 // console.log(incrementString("foo")); // "foo1"
