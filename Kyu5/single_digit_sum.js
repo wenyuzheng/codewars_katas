2f8ff50c3245b2bdeeb8a400449e01190612ca82
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-23 18:39:16 +0000

    single_digit_sum passed

--- Kyu5/single_digit_sum.js
+++ Kyu5/single_digit_sum.js
@@ -1,6 +1,12 @@
 const singleDigitSum = (str) => {
-  const sum = str.split("").reduce((a, b) => parseInt(a) + parseInt(b), 0);
+  const sum = str
+    .split("")
+    .reduce((a, b) => parseInt(a) + parseInt(b), 0)
+    .toString();
+  if (sum.length !== 1) return singleDigitSum(sum);
   return sum;
 };
 
 console.log(singleDigitSum("11"));
+console.log(singleDigitSum("19"));
+console.log(singleDigitSum("34562"));
