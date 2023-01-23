e8a709347da3c7702e4440be5d45a04e413619c7
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-23 18:57:04 +0000

    String_incrementer pass 4 tests

--- Kyu5/string_incrementer.js
+++ Kyu5/string_incrementer.js
@@ -1,10 +1,14 @@
 function incrementString(strng) {
-  let incrementedStr = (parseInt(strng) + 1).toString();
-  if (incrementedStr.length < strng.length) {
-    incrementedStr = incrementedStr.padStart(strng.length, "0");
+  const split = strng.match(/[a-zA-Z]+|[0-9]+/g);
+  const lastNum = split[split.length - 1];
+  let incrementedStr = (parseInt(lastNum) + 1).toString();
+  if (incrementedStr.length < lastNum.length) {
+    incrementedStr = incrementedStr.padStart(lastNum.length, "0");
   }
 
-  return incrementedStr;
+  split[split.length - 1] = incrementedStr;
+
+  return split.reduce((a, b) => a + b);
 }
 
 console.log(incrementString("1")); // "2"
@@ -12,3 +16,4 @@ console.log(incrementString("009")); // "010"
 console.log(incrementString("999")); // "1000"
 
 // console.log(incrementString("foo")); // "foo1"
+console.log(incrementString("fo99obar99")); // "fo99obar100"
