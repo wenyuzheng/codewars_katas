28fdc21fc0a797dd3c877b5ca92204316d892749
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-24 19:32:20 +0000

    string_incrementer pass tests

--- Kyu5/string_incrementer.js
+++ Kyu5/string_incrementer.js
@@ -1,11 +1,13 @@
 function incrementString(strng) {
   const split = strng.match(/[a-zA-Z]+|[0-9]+/g);
-  const lastNum = split[split.length - 1];
-  let incrementedStr = (parseInt(lastNum) + 1).toString();
-  if (incrementedStr.length < lastNum.length) {
-    incrementedStr = incrementedStr.padStart(lastNum.length, "0");
+  if (split.length === 1 && !parseInt(split[0])) {
+    return strng + "1";
   }
-
+  const lastNumStr = split[split.length - 1];
+  let incrementedStr = (parseInt(lastNumStr) + 1).toString();
+  if (incrementedStr.length < lastNumStr.length) {
+    incrementedStr = incrementedStr.padStart(lastNumStr.length, "0");
+  }
   split[split.length - 1] = incrementedStr;
 
   return split.reduce((a, b) => a + b);
@@ -15,5 +17,5 @@ console.log(incrementString("1")); // "2"
 console.log(incrementString("009")); // "010"
 console.log(incrementString("999")); // "1000"
 
-// console.log(incrementString("foo")); // "foo1"
-console.log(incrementString("fo99obar99")); // "fo99obar100"
+console.log(incrementString("foo")); // "foo1"
+console.log(incrementString("fo99obar0099")); // "fo99obar0100"
