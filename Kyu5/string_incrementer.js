c35cdc6d9a04387068a909dbacdba4ed15eaf11c
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-25 01:40:18 +0000

    string_incrementer improved

--- Kyu5/string_incrementer.js
+++ Kyu5/string_incrementer.js
@@ -1,16 +1,17 @@
 function incrementString(strng) {
-  const split = strng.match(/[a-zA-Z]+|[0-9]+/g);
-  if (split.length === 1 && !parseInt(split[0])) {
-    return strng + "1";
-  }
-  const lastNumStr = split[split.length - 1];
-  let incrementedStr = (parseInt(lastNumStr) + 1).toString();
-  if (incrementedStr.length < lastNumStr.length) {
-    incrementedStr = incrementedStr.padStart(lastNumStr.length, "0");
-  }
-  split[split.length - 1] = incrementedStr;
+  return strng.replace(/([0-8]|\d?9+)?$/, (e) => (e ? +e + 1 : 1));
+  // const split = strng.match(/[a-zA-Z]+|[0-9]+/g);
+  // if (split.length === 1 && !parseInt(split[0])) {
+  //   return strng + "1";
+  // }
+  // const lastNumStr = split[split.length - 1];
+  // let incrementedStr = (parseInt(lastNumStr) + 1).toString();
+  // if (incrementedStr.length < lastNumStr.length) {
+  //   incrementedStr = incrementedStr.padStart(lastNumStr.length, "0");
+  // }
+  // split[split.length - 1] = incrementedStr;
 
-  return split.reduce((a, b) => a + b);
+  // return split.reduce((a, b) => a + b);
 }
 
 console.log(incrementString("1")); // "2"
