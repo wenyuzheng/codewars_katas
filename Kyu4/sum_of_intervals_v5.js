04633b4a84549306c6e8650df73624851ba2011d
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-26 23:05:12 +0000

    sum_of_intervals_v5 in progress

--- Kyu4/sum_of_intervals_v5.js
+++ Kyu4/sum_of_intervals_v5.js
@@ -1,36 +1,22 @@
 const sumIntervals = (intervals) => {
   intervals.sort((a, b) => a[0] - b[0]);
 
+  // intervals.forEach((currInterval) => {
+  //   if (currInterval)
+  // });
+
   console.log({ intervals });
 };
 
-const addOneInterval = (intervals, newInterval) => {
-  if (intervals.length === 0) {
-    intervals.push(newInterval);
-    return intervals;
-  }
-
-  intervals.forEach((currInterval) => {});
-};
-
-const addIntervals = (intervals) => {
-  let result = [];
-  while (intervals.length > 0) {
-    const newInterval = intervals.shift();
-    result = addOneInterval(result, newInterval);
-  }
-  return result;
-};
-
-// console.log(
-//   sumIntervals([
-//     [-10, -6],
-//     [9, 16],
-//     [10, 18],
-//     [-1, 4],
-//     [13, 22],
-//     [-17, -8], // 29
-//     [-3, 7],
-//     [-1, 6],
-//   ])
-// ); // 34
+console.log(
+  sumIntervals([
+    [-10, -6],
+    [9, 16],
+    [10, 18],
+    [-1, 4],
+    [13, 22],
+    [-17, -8], // 29
+    [-3, 7],
+    [-1, 6],
+  ])
+); // 34
