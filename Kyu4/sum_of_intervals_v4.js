10205131562af2e555ee4952c901c271c08a4f45
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-26 22:07:54 +0000

    sum_of_intervals_v4

--- Kyu4/sum_of_intervals_v4.js
+++ Kyu4/sum_of_intervals_v4.js
@@ -62,13 +62,24 @@ const addOneIntervals = (intervals, newInterval) => {
 
 const unionIntervals = (intervals) => {
   let result = [];
-  let newInterval;
   while (intervals.length > 0) {
-    newInterval = intervals.shift();
+    const newInterval = intervals.shift();
+    console.log({ newInterval });
     result = addOneIntervals(result, newInterval);
   }
   return result;
 };
+
+console.log(
+  unionIntervals([
+    [1, 3],
+    // [4, 6],
+    // [7, 8],
+    // [9, 11],
+    // [13, 15],
+    // [17, 19],
+  ])
+);
 
 // console.log(findPosition([[1, 4]], 2)); // ["IN", 0]
 // console.log(
@@ -116,68 +127,68 @@ const unionIntervals = (intervals) => {
 //   )
 // ); // [[1,3],[4,15],[17,19]]
 
-console.log(
-  addOneIntervals(
-    [
-      [1, 3],
-      [4, 6],
-      [7, 8],
-      [9, 11],
-      [13, 15],
-      [17, 19],
-    ],
-    [5, 14]
-  )
-); // [[1,3], [4,15], [17,19]]
-// start = 5 either inside an existing interval, or outside
-// end = 14 either inside an existing interval, or outside
-// start is IN intervals[1], end is IN intervals[4]
-// => merge intervals[1] to intervas[4] => [start of Int_1, end of Int_4]
+// console.log(
+//   addOneIntervals(
+//     [
+//       [1, 3],
+//       [4, 6],
+//       [7, 8],
+//       [9, 11],
+//       [13, 15],
+//       [17, 19],
+//     ],
+//     [5, 14]
+//   )
+// ); // [[1,3], [4,15], [17,19]]
+// // start = 5 either inside an existing interval, or outside
+// // end = 14 either inside an existing interval, or outside
+// // start is IN intervals[1], end is IN intervals[4]
+// // => merge intervals[1] to intervas[4] => [start of Int_1, end of Int_4]
 
-console.log(
-  addOneIntervals(
-    [
-      [1, 3],
-      [4, 5],
-      [7, 8],
-      [9, 11],
-      [13, 15],
-      [17, 19],
-    ],
-    [6, 16]
-  )
-); // [[1,3],[4,5],[6,16],[17,19]]
-// start is AF intervals[1], end is AF intervals[4]
-// => replace [2] to [4] with newInterval
+// console.log(
+//   addOneIntervals(
+//     [
+//       [1, 3],
+//       [4, 5],
+//       [7, 8],
+//       [9, 11],
+//       [13, 15],
+//       [17, 19],
+//     ],
+//     [6, 16]
+//   )
+// ); // [[1,3],[4,5],[6,16],[17,19]]
+// // start is AF intervals[1], end is AF intervals[4]
+// // => replace [2] to [4] with newInterval
 
-console.log(
-  addOneIntervals(
-    [
-      [1, 3],
-      [4, 5],
-      [7, 8],
-      [9, 11],
-      [13, 15],
-      [17, 19],
-    ],
-    [6, 14]
-  )
-); // [[1,3],[4,5],[6,15],[17,19]]
-// start is BETWEEN intervals[1][2], end is IN intervals[4]
-// => replace [2] to [4] with [newInterval[0], intervals[4][1]]
+// console.log(
+//   addOneIntervals(
+//     [
+//       [1, 3],
+//       [4, 5],
+//       [7, 8],
+//       [9, 11],
+//       [13, 15],
+//       [17, 19],
+//     ],
+//     [6, 14]
+//   )
+// ); // [[1,3],[4,5],[6,15],[17,19]]
+// // start is BETWEEN intervals[1][2], end is IN intervals[4]
+// // => replace [2] to [4] with [newInterval[0], intervals[4][1]]
 
-console.log(
-  addOneIntervals(
-    [
-      [1, 3],
-      [4, 5],
-      [7, 8],
-      [9, 11],
-      [13, 15],
-      [17, 19],
-    ],
-    [4.5, 16]
-  )
-); // [[1,3],[4,16],[17,19]]
-// // start is IN intervals[1], end is BETWEEN intervals[4][5]
-// // => replace [1] to [4] with [intervals[1][0], newInterval[1]]
+// console.log(
+//   addOneIntervals(
+//     [
+//       [1, 3],
+//       [4, 5],
+//       [7, 8],
+//       [9, 11],
+//       [13, 15],
+//       [17, 19],
+//     ],
+//     [4.5, 16]
+//   )
+// ); // [[1,3],[4,16],[17,19]]
+// // // start is IN intervals[1], end is BETWEEN intervals[4][5]
+// // // => replace [1] to [4] with [intervals[1][0], newInterval[1]]
