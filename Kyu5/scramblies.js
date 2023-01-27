7d4e1e8ab84fcf7315abef9ff02461d88e39790c
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-27 16:08:05 +0000

    scramblies works but slow

--- Kyu5/scramblies.js
+++ Kyu5/scramblies.js
@@ -1,7 +1,12 @@
 function scramble(str1, str2) {
-  return false;
+  for (let i = 0; i < str2.length; i++) {
+    console.log(str1.indexOf(str2[i]));
+    if (!str1.includes(str2[i])) return false;
+    str1 = str1.replace(str2[i], "");
+  }
+  return true;
 }
 
-console.log(scramble("rkqodlw", "world")); // true
-console.log(scramble("cedewaraaossoqqyt", "codewars")); // true
+// console.log(scramble("rkqodlw", "world")); // true
+// console.log(scramble("cedewaraaossoqqyt", "codewars")); // true
 console.log(scramble("katas", "steak")); // false
