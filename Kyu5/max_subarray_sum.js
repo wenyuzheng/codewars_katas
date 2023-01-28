a7ae1cebda03091d39509e898739bf299ac927b0
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-28 22:28:41 +0000

    Join numbers

--- Kyu5/max_subarray_sum.js
+++ Kyu5/max_subarray_sum.js
@@ -10,16 +10,30 @@ var removeLeadOrEndNegatives = function (arr) {
   return arr;
 };
 
+var joinNumbers = function (arr) {
+  for (let i = 1; i < arr.length; i++) {
+    if (
+      (arr[i] > 0 && arr[i - 1] > 0) ||
+      (arr[i] < 0 && arr[i - 1] < 0) ||
+      arr[i] + arr[i - 1] > 0
+    ) {
+      arr.splice(i - 1, 2, arr[i] + arr[i - 1]);
+      return joinNumbers(arr);
+    }
+  }
+  return arr;
+};
+
 var maxSequence = function (arr) {
   if (arr.length === 0) return 0;
   arr = removeLeadOrEndNegatives(arr);
-  console.log({ arr });
-  for (let i = 0; i < arr.length; i++) {}
 };
 
-console.log(maxSequence([])); // 0
-console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // 6
+// console.log(maxSequence([])); // 0
+// console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // 6
 
-console.log(
-  removeLeadOrEndNegatives([0, -3, -2, 1, -3, 4, -1, 2, 1, -5, 4, -1, -2])
-);
+// console.log(
+//   removeLeadOrEndNegatives([0, -3, -2, 1, -3, 4, -1, 2, 1, -5, 4, -1, -2])
+// );
+
+console.log(joinNumbers([1, -3, -2, 4, 4, 5, -10, -1, 2, 1, -5, 4]));
