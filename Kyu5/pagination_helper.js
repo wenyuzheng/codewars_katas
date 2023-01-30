e974c1a66e67de03dd0a20dfc04dd8e94f2557ba
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-30 01:39:23 +0000

    Pass all otehr methods tests

--- Kyu5/pagination_helper.js
+++ Kyu5/pagination_helper.js
@@ -2,20 +2,31 @@ class PaginationHelper {
   constructor(collection, itemsPerPage) {
     // The constructor takes in an array of items and a integer indicating how many
     // items fit within a single page
+    this.collection = collection;
+    this.itemsPerPage = itemsPerPage;
   }
   itemCount() {
     // returns the number of items within the entire collection
+    return this.collection.length;
   }
   pageCount() {
     // returns the number of pages
+    return Math.ceil(this.itemCount() / this.itemsPerPage);
   }
   pageItemCount(pageIndex) {
     // returns the number of items on the current page. page_index is zero based.
     // this method should return -1 for pageIndex values that are out of range
+
+    const totalPages = this.pageCount() - 1; //start from 0
+
+    if (pageIndex > totalPages) return -1;
+    if (pageIndex === totalPages) return this.itemCount() % this.itemsPerPage;
+    return this.itemsPerPage;
   }
   pageIndex(itemIndex) {
     // determines what page an item is on. Zero based indexes
     // this method should return -1 for itemIndex values that are out of range
+    if (1 < itemIndex || itemIndex < this.itemCount()) return -1;
   }
 }
 
@@ -26,8 +37,8 @@ console.log(helper.pageItemCount(0)); //should == 4
 console.log(helper.pageItemCount(1)); // last page - should == 2
 console.log(helper.pageItemCount(2)); // should == -1 since the page is invalid
 
-// pageIndex takes an item index and returns the page that it belongs on
-console.log(helper.pageIndex(5)); //should == 1 (zero based index)
-console.log(helper.pageIndex(2)); //should == 0
+// // pageIndex takes an item index and returns the page that it belongs on
+// console.log(helper.pageIndex(5)); //should == 1 (zero based index)
+// console.log(helper.pageIndex(2)); //should == 0
 console.log(helper.pageIndex(20)); //should == -1
 console.log(helper.pageIndex(-10)); //should == -1
