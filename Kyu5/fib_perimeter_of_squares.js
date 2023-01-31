533f293e4fb480d84e13f37c5c87f66275dcbe9a
Wenyu Zheng <zhengwy229@gmail.com>
2023-01-31 18:06:07 +0000

    pass init test

---
+++ Kyu5/fib_perimeter_of_squares.js
@@ -0,0 +1,21 @@
+function perimeter(n) {
+  let sum = 0;
+  for (let i = 0; i <= n; i++) {
+    console.log(fib(n));
+    sum += fib(n);
+  }
+  return 4 * sum;
+}
+
+function fib(n) {
+  if (n === 0 || n === 1) return 1;
+  return fib(n - 1) + fib(n - 2);
+}
+
+// console.log(fib(5));
+
+console.log(perimeter(0)); // 4
+// console.log(perimeter(5)); // 80
+// console.log(perimeter(7)); // 216
+// console.log(perimeter(20)); // 114624
+// console.log(perimeter(30)); // 14098308
