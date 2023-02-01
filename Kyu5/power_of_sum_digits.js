84e5b7e98750172d80e295767052010c459a4b78
Wenyu Zheng <zhengwy229@gmail.com>
2023-02-01 23:33:37 +0000

    Come back later

---
+++ Kyu5/power_of_sum_digits.js
@@ -0,0 +1,9 @@
+function powerSumDigTerm(n) {
+  // either sq_rt or cube_rt
+  if (n === 1) return 81;
+}
+
+console.log(powerSumDigTerm(1)); // 81 => 9^2
+console.log(powerSumDigTerm(2)); // 512 => 8^3
+console.log(powerSumDigTerm(3)); // 2401 => 7^4 => 49^2
+console.log(powerSumDigTerm(4)); // 4913 => 17^3
