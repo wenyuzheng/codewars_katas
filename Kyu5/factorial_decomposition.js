d88d827d0859a39dff7762187d755c81ab699bd5
Wenyu Zheng <zhengwy229@gmail.com>
2023-02-04 14:27:58 +0000

    in progress

--- Kyu5/factorial_decomposition.js
+++ Kyu5/factorial_decomposition.js
@@ -1,16 +1,16 @@
 function decomp(n) {
   const primes = { 2: 0, 3: 0, 5: 0, 7: 0, 9: 0, 11: 0 };
   for (let i = 2; i <= n; i++) {
-    Object.keys(primes).reduce((k) => {
-      if (i % k) primes[k]++;
-    });
+    // Object.keys(primes).reduce((k) => {
+    //   if (i % k) primes[k]++;
+    // });
   }
   let str = "";
   Object.keys(primes).reduce((k) => {
-    if (primes[k] != 0) str += `${k}^${primes[k]}`;
+    if (primes[k] != 0) str += `${k}^${primes[k]} *`;
   });
   return str;
 }
 
 console.log(decomp(2)); // "2^1"
-console.log(decomp(5)); // "2^3 * 3 * 5"
+// console.log(decomp(5)); // "2^3 * 3 * 5"
