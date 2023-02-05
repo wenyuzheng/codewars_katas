20dc4f9d7029a6b5bf548b5ca443540dfe2b3f8b
Wenyu Zheng <zhengwy229@gmail.com>
2023-02-05 01:09:29 +0000

    sumConditional recursion done

--- recursion/sumConditional.js
+++ recursion/sumConditional.js
@@ -1,4 +1,9 @@
-const sumConditional = (numArr, conditionFunc) => {};
+const sumConditional = (arr, conditionFunc, res = 0) => {
+  if (arr.length === 0) return res;
+  const e = arr.shift();
+  res += conditionFunc(e) ? e : 0;
+  return sumConditional(arr, conditionFunc, res);
+};
 
 const isMultipleOf3 = (n) => n % 3 === 0;
 console.log(sumConditional([1, 2, 3, 4, 5, 6], isMultipleOf3)); // 3+6 =9
