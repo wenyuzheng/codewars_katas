aeb99006fb20d7bde9ae2102df5de128743215b1
Wenyu Zheng <zhengwy229@gmail.com>
2023-02-05 00:39:45 +0000

    sumOfNumbers recursion done

--- recursion/sumOfNumbers.js
+++ recursion/sumOfNumbers.js
@@ -1,4 +1,8 @@
-const sumOfNumbers = (arr) => {};
+const sumOfNumbers = (arr, res = 0) => {
+  if (arr.length === 0) return res;
+  res += arr.shift();
+  return sumOfNumbers(arr, res);
+};
 
 console.log(sumOfNumbers([1, 2, 3]));
 
