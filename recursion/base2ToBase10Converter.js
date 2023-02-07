df428edb68a9b5f438dd0af979ddca38e247cda7
Wenyu Zheng <zhengwy229@gmail.com>
2023-02-07 16:18:25 +0000

    base2ToBase10 recursion pass tests

--- recursion/base2ToBase10Converter.js
+++ recursion/base2ToBase10Converter.js
@@ -1,19 +1,22 @@
-// const base2ToBase10Converter = (num, res = 0) => {
-//   if (num === 0) return res;
+const base2ToBase10Converter = (num, i = 0, res = 0) => {
+  if (num === 0) return res;
 
-//   let i = 0;
-//   while (num >= 3 ** i) i++;
-//   return base2ToBase10Converter(num - 3 ** (i - 1), res + 10 ** (i - 1));
-// };
+  const numArr = num.toString().split("");
+  res += 2 ** i * parseInt(numArr.pop());
+  num = numArr.length === 0 ? 0 : parseInt(numArr.join(""));
+  i++;
 
-const base2ToBase10Converter = (num, res = 0) => {
-  const arr = num.toString().split("");
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum += 2 ** (arr.length - 1 - i) * parseInt(arr[i]);
-  }
-  return sum;
+  return base2ToBase10Converter(num, i, res);
 };
+
+// const base2ToBase10Converter = (num, res = 0) => {
+//   const arr = num.toString().split("");
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += 2 ** (arr.length - 1 - i) * parseInt(arr[i]);
+//   }
+//   return sum;
+// };
 
 console.log(base2ToBase10Converter(1)); // 1
 console.log(base2ToBase10Converter(10)); // 2
