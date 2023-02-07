a4c26a8d0b3ab8d5ef9459b9f4d6dadf4fc46cce
Wenyu Zheng <zhengwy229@gmail.com>
2023-02-07 17:15:31 +0000

    pass base tests

---
+++ Kyu5/hofstadters_figure_figure_sequence.js
@@ -0,0 +1,7 @@
+function hof(n) {
+  return 0;
+}
+
+console.log(hof(0)); // 1
+console.log(hof(1)); // 3
+console.log(hof(2)); // 7
---
+++ Kyu5/unique_digits_sequence.js
@@ -0,0 +1,11 @@
+function findNum(n) {
+  if (0 <= n && n <= 10) {
+    return n;
+  }
+}
+
+console.log(findNum(1)); // 1
+console.log(findNum(5)); // 5
+console.log(findNum(11)); // 22
+console.log(findNum(100)); // 103
+console.log(findNum(500)); // 476
