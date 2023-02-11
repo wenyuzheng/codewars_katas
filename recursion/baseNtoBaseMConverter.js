d3b75424c48cb2a5561b531899c65b01b35b9089
Wenyu Zheng <zhengwy229@gmail.com>
2023-02-11 14:46:41 +0000

    recursion got decimal

--- recursion/baseNtoBaseMConverter.js
+++ recursion/baseNtoBaseMConverter.js
@@ -8,36 +8,60 @@
 //       // const tempRes = (res + n ** (i - 1)) % m === 0 ? 10 : res + n ** (i - 1);
 //       // console.log({ tempRes });
 //       res += n ** (i - 1);
-//       num -= m ** (i - 1) * n ** (i - 1);
+//       num -= n ** (i - 1);
+
+//       console.log({ res });
+//       console.log({ num });
+
 //       return baseNToBaseMConverter(num, n, m, res);
 //     }
 //     i++;
 //   }
 // };
 
-const baseNToBaseMConverter = (num, n, m) => {
+// const baseNToBaseMConverter = (num, n, m) => {
+//   // base n -> base 10 -> base m
+
+//   let sum = 0;
+//   const numDigits = num.toString().split("").reverse();
+//   for (let i = 0; i < numDigits.length; i++) {
+//     sum += parseInt(numDigits[i]) * n ** i;
+//   }
+
+//   let res = [];
+//   while (sum !== 0) {
+//     res = [sum % m, ...res];
+//     sum = Math.floor(sum / m);
+//   }
+
+//   return parseInt(res.join(""));
+// };
+
+const baseNToBaseMConverter = (num, n, m, res = 0) => {
   // base n -> base 10 -> base m
 
-  let sum = 0;
-  const numDigits = num.toString().split("").reverse();
-  for (let i = 0; i < numDigits.length; i++) {
-    sum += parseInt(numDigits[i]) * n ** i;
-  }
+  if (num === 0) return res;
 
-  let res = [];
-  while (sum !== 0) {
-    res = [sum % m, ...res];
-    sum = Math.floor(sum / m);
-  }
+  let decimal = 0;
+  const digits = num.toString().split("");
+  decimal += n ** (digits.length - 1) * parseInt(digits.shift());
 
-  return parseInt(res.join(""));
+  res += decimal % m;
+
+  num = digits.length === 0 ? 0 : parseInt(digits.join(""));
+
+  // console.log({ decimal });
+  // console.log({ res });
+  // console.log({ num });
+
+  return baseNToBaseMConverter(num, n, m, res);
 };
 
 console.log(baseNToBaseMConverter(101, 2, 5)); // 10
-console.log(baseNToBaseMConverter(101, 2, 3)); // 12
+// console.log(baseNToBaseMConverter(101, 2, 3)); // 12
 
-console.log(baseNToBaseMConverter(100, 2, 5)); // 4
-console.log(baseNToBaseMConverter(1000, 2, 5)); // 13
+// console.log(baseNToBaseMConverter(100, 2, 5)); // 4
+// console.log(baseNToBaseMConverter(1000, 2, 5)); // 13
 
 // 101 5
 
