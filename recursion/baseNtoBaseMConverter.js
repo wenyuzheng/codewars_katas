eae601437cfde6b787734306c6ddac05669c1ea2
Wenyu Zheng <zhengwy229@gmail.com>
2023-02-11 13:57:40 +0000

    starting new method

--- recursion/baseNtoBaseMConverter.js
+++ recursion/baseNtoBaseMConverter.js
@@ -1,24 +1,33 @@
-const baseNToBaseMConverter = (num, n, m, res = 0) => {
-  if (num == 0) return res;
-  let i = 0;
-  while (true) {
-    console.log({ i });
-    console.log(m ** i);
-    if (num < m ** i) {
-      console.log(n ** (i - 1));
-      console.log(num - m ** (i - 1) * n ** (i - 1));
+// const baseNToBaseMConverter = (num, n, m, res = 0) => {
+//   // if (num == 0) return parseInt(res.join(""));
+//   if (num == 0) return res;
 
-      res += n ** (i - 1);
-      num -= m ** (i - 1) * n ** (i - 1);
-      console.log({ res });
-      console.log({ num });
-      return baseNToBaseMConverter(num, n, m, res);
-    }
-    i++;
-  }
+//   let i = 0;
+//   while (true) {
+//     if (num < m ** i) {
+//       // const tempRes = (res + n ** (i - 1)) % m === 0 ? 10 : res + n ** (i - 1);
+//       // console.log({ tempRes });
+//       res += n ** (i - 1);
+//       num -= m ** (i - 1) * n ** (i - 1);
+//       return baseNToBaseMConverter(num, n, m, res);
+//     }
+//     i++;
+//   }
+// };
+
+const baseNToBaseMConverter = (num, n, m, res = 0) => {
+  // base n -> base 10 -> base m
+
+  if (num === 0) return res;
+
+  num.toString().split("");
 };
 
-console.log(baseNToBaseMConverter(101, 2, 5)); // 10
+// console.log(baseNToBaseMConverter(101, 2, 5)); // 10
+console.log(baseNToBaseMConverter(101, 2, 3)); // 12
+
+// console.log(baseNToBaseMConverter(100, 2, 5)); // 4
+// console.log(baseNToBaseMConverter(1000, 2, 5)); // 13
 
 // 101 5
 
@@ -26,3 +35,7 @@ console.log(baseNToBaseMConverter(101, 2, 5)); // 10
 // 100 base2 = 04 base5
 // 1 base2 = 01 base5
 // 01 + 04 = 10 base5
+
+// 101 = 100 + 1
+// 100 base2 = 11 base3
+// 1 base2 = 1 base3
