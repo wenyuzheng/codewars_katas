3955f31ea686619d5aa8f76fc02b67136672356c
Wenyu Zheng <zhengwy229@gmail.com>
2023-02-19 00:42:24 +0000

    Pass all tests

--- Kyu4/range_extraction.js
+++ Kyu4/range_extraction.js
@@ -2,33 +2,21 @@ function solution(list) {
   let res = "";
   let range = [];
 
-  //   list.forEach((e) => (res += e + ","));
-
   for (let i = 0; i < list.length; i++) {
     if (range.length === 0) {
-      console.log("range is empty");
       range.push(list[i]);
     } else {
-      //   console.log(range[range.length - 1]);
-      //   console.log(range[range.length - 1] + 1);
-      //   console.log(list[i]);
-      //   console.log(range[range.length - 1] + 1 === list[i]);
-      //   console.log(range[range.length - 1] + 1 !== list[i]);
-
       if (range[range.length - 1] + 1 === list[i]) {
-        console.log("is consecutive");
         range.push(list[i]);
       } else {
-        // if (range.length >= 3) {
-        //   res += range[0] + "-" + range[range.length - 1] + ",";
-        // }
-        // else {
-        range.forEach((e) => (res += e + ","));
-        // }
+        if (range.length >= 3) {
+          res += range[0] + "-" + range[range.length - 1] + ",";
+        } else {
+          range.forEach((e) => (res += e + ","));
+        }
         range = [list[i]];
       }
     }
-    console.log({ range });
   }
   if (range.length !== 0) {
     if (range.length >= 3) {
@@ -43,8 +31,8 @@ function solution(list) {
 console.log(solution([10, 11, 14, 15])); // "10,11,14,15,"
 console.log(solution([17, 18, 19, 20])); // "17-20"
 
-// console.log(
-//   solution([
-//     -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20,
-//   ])
-// ); // "-6,-3-1,3-5,7-11,14,15,17-20"
+console.log(
+  solution([
+    -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20,
+  ])
+); // "-6,-3-1,3-5,7-11,14,15,17-20"
