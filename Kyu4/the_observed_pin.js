79bae8a1b689cf3d7bad5eea637821212ae26b41
Wenyu Zheng <zhengwy229@gmail.com>
2023-02-20 22:09:57 +0000

    Pass all tests

--- Kyu4/the_observed_pin.js
+++ Kyu4/the_observed_pin.js
@@ -39,16 +39,11 @@ const joinElements = (array) => {
   if (array.length === 1) return array[0];
 
   let res = [];
-  for (let j = 0; j < array.length - 1; j++) {
-    const arr1 = array.shift();
-    const arr2 = array.shift();
-    for (let i = 0; i < arr1.length; i++) {
-      arr2.forEach((e) => {
-        res.push(arr1[i] + e);
-      });
-    }
-  }
-
+  const arr1 = array.shift();
+  const arr2 = array.shift();
+  arr1.forEach((e1) => {
+    arr2.forEach((e2) => res.push(e1 + e2));
+  });
   return joinElements([res, ...array]);
 };
 
@@ -67,9 +62,11 @@ function getPINs(observed) {
   digits.forEach((digit) => {
     adjacents.push(getAdjacents(digit));
   });
+
   return joinElements(adjacents);
 }
 
 console.log(getPINs("8")); // ["5", "7", "8", "9", "0"]
 console.log(getPINs("11")); // ["11", "22", "44", "12", "21", "14", "41", "24", "42"]
 console.log(getPINs("369")); //  ["339","366","399","658","636","258","268","669","668","266","369","398","256","296","259","368","638","396","238","356","659","639","666","359","336","299","338","696","269","358","656","698","699","298","236","239"]
+console.log(getPINs("24680"));
