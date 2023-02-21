3caa2f9e4ad12c73a0fb1404f29b892ac740f53e
Wenyu Zheng <zhengwy229@gmail.com>
2023-02-21 22:30:43 +0000

    Pass all tests

--- Kyu4/nesting_structure_comparison.js
+++ Kyu4/nesting_structure_comparison.js
@@ -13,17 +13,19 @@ Array.prototype.sameStructureAs = function (other) {
 
   for (let i = 0; i < this.length; i++) {
     if (Array.isArray(this[i]) !== Array.isArray(other[i])) return false;
+    else if (Array.isArray(this[i])) {
+      return this[i].sameStructureAs(other[i]);
+    }
   }
-
   return true;
 };
 
-// console.log([1, 1, 1].sameStructureAs([2, 2, 2])); // true
-// console.log([1, [1, 1]].sameStructureAs([2, [2, 2]])); // true
-// console.log([1, "[", "]"].sameStructureAs(["[", "]", 1])); // true
+console.log([1, 1, 1].sameStructureAs([2, 2, 2])); // true
+console.log([1, [1, 1]].sameStructureAs([2, [2, 2]])); // true
+console.log([1, "[", "]"].sameStructureAs(["[", "]", 1])); // true
 
-// console.log([1, [1, 1]].sameStructureAs([[2, 2], 2])); // false
-// console.log([1, 2].sameStructureAs([[3], 3])); // false
-// console.log([].sameStructureAs(1)); // false
-// console.log([].sameStructureAs({})); // false
+console.log([1, [1, 1]].sameStructureAs([[2, 2], 2])); // false
+console.log([1, 2].sameStructureAs([[3], 3])); // false
+console.log([].sameStructureAs(1)); // false
+console.log([].sameStructureAs({})); // false
 console.log([[[], []]].sameStructureAs([[1, 1]])); // false
