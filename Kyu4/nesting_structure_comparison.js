4c45c37d02a8c530e10ca66d83428a9e83238247
Wenyu Zheng <zhengwy229@gmail.com>
2023-02-21 22:20:14 +0000

    Pass 7 tests

---
+++ Kyu4/nesting_structure_comparison.js
@@ -0,0 +1,29 @@
+Array.prototype.sameStructureAs = function (other) {
+  // Return 'true' if and only if 'other' has the same
+  // nesting structure as 'this'.
+  // Note: You are given a function isArray(o) that returns
+  // whether its argument is an array.
+
+  if (
+    !Array.isArray(this) ||
+    !Array.isArray(other) ||
+    this.length !== other.length
+  )
+    return false;
+
+  for (let i = 0; i < this.length; i++) {
+    if (Array.isArray(this[i]) !== Array.isArray(other[i])) return false;
+  }
+
+  return true;
+};
+
+// console.log([1, 1, 1].sameStructureAs([2, 2, 2])); // true
+// console.log([1, [1, 1]].sameStructureAs([2, [2, 2]])); // true
+// console.log([1, "[", "]"].sameStructureAs(["[", "]", 1])); // true
+
+// console.log([1, [1, 1]].sameStructureAs([[2, 2], 2])); // false
+// console.log([1, 2].sameStructureAs([[3], 3])); // false
+// console.log([].sameStructureAs(1)); // false
+// console.log([].sameStructureAs({})); // false
+console.log([[[], []]].sameStructureAs([[1, 1]])); // false
