82f068fc759f77042bf389c1957163abc5985c4b
Wenyu Zheng <zhengwy229@gmail.com>
2023-02-23 21:14:31 +0000

    Almost

--- Kyu4/strings_mix.js
+++ Kyu4/strings_mix.js
@@ -34,7 +34,11 @@ function mix(s1, s2) {
   let str = [];
   for (let i = 0; i < lowercases.length; i++) {
     const e = lowercases[i];
-    count = Math.max(
+    if (s1Letters[e] === s2Letters[e] && s1Letters[e] > 1) {
+      str.push("=" + ":" + e.repeat(s1Letters[e]));
+      continue;
+    }
+    const count = Math.max(
       s1Letters[e] ? s1Letters[e] : 0,
       s2Letters[e] ? s2Letters[e] : 0
     );
@@ -42,6 +46,23 @@ function mix(s1, s2) {
     const origin = count === s1Letters[e] ? "1" : "2";
     if (count > 1) str.push(origin + ":" + e.repeat(count));
   }
+
+  //   for (let i = 0; i < lowercases.length; i++) {
+  //     const e = lowercases[i];
+  //     if (s1Letters[e] === s2Letters[e] && s1Letters[e] > 1) {
+  //       str.push({ from: "=", string: e.repeat(s1Letters[e]) });
+  //       continue;
+  //     }
+  //     const count = Math.max(
+  //       s1Letters[e] ? s1Letters[e] : 0,
+  //       s2Letters[e] ? s2Letters[e] : 0
+  //     );
+
+  //     const origin = count === s1Letters[e] ? "1" : "2";
+  //     if (count > 1) str.push({ from: origin, string: e.repeat(count) });
+  //   }
+
+  str.sort((a, b) => b.slice(2).length - a.slice(2).length);
 
   return str.join("/");
 }
@@ -56,8 +77,14 @@ const countLetters = (str) => {
   return letters;
 };
 
-console.log(countLetters("A aaaa bb c")); // 4 'a', 2 'b', 1 'c'
-console.log(countLetters("& aaa bbb c d")); // 3 'a', 3 'b', 1 'c', 1 'd'
+// console.log(countLetters("A aaaa bb c")); // 4 'a', 2 'b', 1 'c'
+// console.log(countLetters("& aaa bbb c d")); // 3 'a', 3 'b', 1 'c', 1 'd'
 
-console.log(mix("A aaaa bb c", "& aaa bbb c d")); // "1:aaaa/2:bbb"
+// console.log(mix("A aaaa bb c", "& aaa bbb c d")); // "1:aaaa/2:bbb"
 // console.log(mix("Are they here", "yes, they are here")); // "2:eeeee/2:yy/=:hh/=:rr"
+console.log(
+  mix(
+    "my&friend&Paul has heavy hats! &",
+    "my friend John has many many friends &"
+  )
+); // "2:nnnnn/1:aaaa/1:hhh/2:mmm/2:yyy/2:dd/2:ff/2:ii/2:rr/=:ee/=:ss"
