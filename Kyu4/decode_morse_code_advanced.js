f38c127b792cfdada4b8a27c1bcbcbd7ddf351a1
Wenyu Zheng <zhengwy229@gmail.com>
2023-02-23 01:36:51 +0000

    Not working

--- Kyu4/decode_morse_code_advanced.js
+++ Kyu4/decode_morse_code_advanced.js
@@ -38,31 +38,37 @@ const MORSE_CODE = {
 };
 
 var decodeBits = function (bits) {
-  let rate = Infinity;
+  //   const splitBit = bits.includes("0") ? "1" : "0";
+
+  //   console.log({ splitBit });
+
+  let longestLength = 0;
 
   bits
     .split("0")
     .filter((e) => e !== "")
-    .forEach((e) => (rate = Math.min(rate, e.length)));
+    .forEach((e) => (longestLength = Math.max(longestLength, e.length)));
 
-  return bits
-    .split("0000000".repeat(rate))
-    .map((word) =>
-      word
-        .split("000".repeat(rate))
-        .map((char) =>
-          char
-            .split("0".repeat(rate))
-            .map((symbol) =>
-              symbol
-                .replace("111".repeat(rate), "-")
-                .replace("1".repeat(rate), ".")
-            )
-            .join("")
-        )
-        .join(" ")
-    )
-    .join("   ");
+  return longestLength % 3;
+
+  //   return bits
+  //     .split("0000000".repeat(rate))
+  //     .map((word) =>
+  //       word
+  //         .split("000".repeat(rate))
+  //         .map((char) =>
+  //           char
+  //             .split("0".repeat(rate))
+  //             .map((symbol) =>
+  //               symbol
+  //                 .replace("111".repeat(rate), "-")
+  //                 .replace("1".repeat(rate), ".")
+  //             )
+  //             .join("")
+  //         )
+  //         .join(" ")
+  //     )
+  //     .join("   ");
 };
 
 decodeMorse = function (morseCode) {
@@ -78,22 +84,26 @@ decodeMorse = function (morseCode) {
     .join(" ");
 };
 
-console.log(
-  decodeMorse(
-    decodeBits(
-      "1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011"
-    )
-  )
-); // "HEY JUDE"
+// console.log(
+//   decodeMorse(
+//     decodeBits(
+//       "1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011"
+//     )
+//   )
+// ); // "HEY JUDE"
 
-("···· · −·−−   ·−−− ··− −·· ·");
-("[11 00 11 00 11 00 11] 000000 [11] 000000 [111111 00 11 00 111111 00 111111] 00000000000000 [11 00 111111 00 111111 00 111111] 000000 [11 00 11 00 111111] 000000 [111111 00 11 00 11] 000000 [11]");
+// console.log(decodeMorse(decodeBits("1"))); // 'E'
+// console.log(decodeMorse(decodeBits("111"))); // 'E'
+// console.log(decodeMorse(decodeBits("1111111"))); // 'E'
 
-console.log(decodeMorse(decodeBits("1"))); // 'E'
-console.log(decodeMorse(decodeBits("111"))); // 'E'
-console.log(decodeMorse(decodeBits("1111111"))); // 'E'
+// console.log(decodeMorse(decodeBits("101"))); // 'I'
+// console.log(decodeMorse(decodeBits("10001"))); // 'EE'
+// console.log(decodeMorse(decodeBits("10111"))); // 'A'
 
-console.log(decodeMorse(decodeBits("101"))); // 'I'
-console.log(decodeMorse(decodeBits("10001"))); // 'EE'
-console.log(decodeMorse(decodeBits("10111"))); // 'A'
+console.log(decodeBits("000000011100000"));
+
 // console.log(decodeMorse(decodeBits("1110111"))); // 'M'
+// console.log(decodeMorse(decodeBits("11111100111111"))); // 'M'
+
+// console.log(decodeMorse(decodeBits("01110"))); // 'E'
+// console.log(decodeMorse(decodeBits("000000011100000"))); // 'E'
