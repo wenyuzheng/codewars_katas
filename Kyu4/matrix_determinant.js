f42faa1997c3110e3b9ac1ed438995e3f8467bc2
Wenyu Zheng <zhengwy229@gmail.com>
2023-02-23 18:29:43 +0000

    Pass all tests

--- Kyu4/matrix_determinant.js
+++ Kyu4/matrix_determinant.js
@@ -1,24 +1,32 @@
 function determinant(m) {
   const n = m.length;
   if (n === 1) return m[0][0];
-  if (n === 2) return twoTimesTwoMatrix(m);
+  if (n === 2) return m[0][0] * m[1][1] - m[0][1] * m[1][0];
+
+  let sum = 0;
+  for (let i = 0; i < n; i++) {
+    const newM = m.map((e) => e.slice());
+    newM.shift();
+    newM.forEach((e) => e.splice(i, 1));
+
+    i % 2 === 0
+      ? (sum += m[0][i] * determinant(newM))
+      : (sum -= m[0][i] * determinant(newM));
+  }
+  return sum;
 }
 
-const twoTimesTwoMatrix = (matrix) => {
-  return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
-};
-
-console.log(determinant([[1]])); // 1
-console.log(
-  determinant([
-    [4, 6],
-    [3, 8],
-  ])
-); // 4*8 - 3*6 = 14
+// console.log(determinant([[1]])); // 1
+// console.log(
+//   determinant([
+//     [4, 6],
+//     [3, 8],
+//   ])
+// ); // 4*8 - 3*6 = 14
 console.log(
   determinant([
     [2, 4, 2],
     [3, 1, 1],
     [1, 2, 0],
   ])
-); // 10
+); // 2*-2 - 4*-1 + 2*5 = -4+4+10 = 10
