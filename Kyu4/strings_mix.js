0c20f044c2ab4e5b03ecc24fdcc0d58e027e04da
Wenyu Zheng <zhengwy229@gmail.com>
2023-02-24 18:38:03 +0000

    pass more tests

--- Kyu4/strings_mix.js
+++ Kyu4/strings_mix.js
@@ -47,22 +47,20 @@ function mix(s1, s2) {
     if (count > 1) str.push(origin + ":" + e.repeat(count));
   }
 
-  //   for (let i = 0; i < lowercases.length; i++) {
-  //     const e = lowercases[i];
-  //     if (s1Letters[e] === s2Letters[e] && s1Letters[e] > 1) {
-  //       str.push({ from: "=", string: e.repeat(s1Letters[e]) });
-  //       continue;
-  //     }
-  //     const count = Math.max(
-  //       s1Letters[e] ? s1Letters[e] : 0,
-  //       s2Letters[e] ? s2Letters[e] : 0
-  //     );
-
-  //     const origin = count === s1Letters[e] ? "1" : "2";
-  //     if (count > 1) str.push({ from: origin, string: e.repeat(count) });
-  //   }
-
-  str.sort((a, b) => b.slice(2).length - a.slice(2).length);
+  str
+    .sort((a, b) => {
+      const order = ["2", "1", "="];
+      const x = a.slice(0, 1);
+      const y = b.slice(0, 1);
+      if (order.indexOf(x) < order.indexOf(y)) {
+        return -1;
+      }
+      if (order.indexOf(x) > order.indexOf(y)) {
+        return 1;
+      }
+      return 0;
+    })
+    .sort((a, b) => b.slice(2).length - a.slice(2).length);
 
   return str.join("/");
 }
@@ -81,7 +79,7 @@ const countLetters = (str) => {
 // console.log(countLetters("& aaa bbb c d")); // 3 'a', 3 'b', 1 'c', 1 'd'
 
 // console.log(mix("A aaaa bb c", "& aaa bbb c d")); // "1:aaaa/2:bbb"
-// console.log(mix("Are they here", "yes, they are here")); // "2:eeeee/2:yy/=:hh/=:rr"
+console.log(mix("Are they here", "yes, they are here")); // "2:eeeee/2:yy/=:hh/=:rr"
 console.log(
   mix(
     "my&friend&Paul has heavy hats! &",
