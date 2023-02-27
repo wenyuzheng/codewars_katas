1fd76ab64bbcd92035036904c651bb4ebf0b2296
Wenyu Zheng <zhengwy229@gmail.com>
2023-02-27 23:27:58 +0000

    pass three tests

--- Kyu4/parseInt_reloaded.js
+++ Kyu4/parseInt_reloaded.js
@@ -19,20 +19,40 @@ const numStrings = {
   eighteen: 18,
   nineteen: 19,
   twenty: 20,
+  thirty: 30,
+  forty: 40,
+  fifty: 50,
+  sixty: 60,
+  seventy: 70,
+  eighty: 80,
+  ninety: 90,
   hundred: 100,
   thousand: 1000,
 };
 
 function parseInt(string) {
-  return string
+  const numArr = string
+    .replace("-", " ")
     .split(" ")
     .filter((x) => x !== "and")
     .map((e) => numStrings[e]);
+
+  let sum = 0;
+  for (let i = 0; i < numArr.length; i++) {
+    if (numArr[i] === 100) {
+      sum += numArr[i - 1] * 100;
+      sum -= numArr[i - 1];
+    } else {
+      sum += numArr[i];
+    }
+  }
+
+  return sum;
 }
 
-// console.log(parseInt("one")); // 1
-// console.log(parseInt("twenty")); // 20
+console.log(parseInt("one")); // 1
+console.log(parseInt("twenty")); // 20
 console.log(parseInt("two hundred forty-six")); // 246
-console.log(
-  parseInt("seven hundred eighty-three thousand nine hundred and nineteen")
-); // 783919
+// console.log(
+//   parseInt("seven hundred eighty-three thousand nine hundred and nineteen")
+// ); // 783919
