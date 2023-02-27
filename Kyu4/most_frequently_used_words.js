4d234f313e0c5a029ed20a54f772ec34edca3b65
Wenyu Zheng <zhengwy229@gmail.com>
2023-02-27 23:01:33 +0000

    new version

--- Kyu4/most_frequently_used_words.js
+++ Kyu4/most_frequently_used_words.js
@@ -1,21 +1,38 @@
+// function topThreeWords(text) {
+//   const strArr = text
+//     .split(" ")
+//     .map((e) => e.toLowerCase().replace(/[^a-z']|/g, ""))
+//     .filter((x) => x !== "'" && x !== "");
+
+//   if (strArr.length <= 1) return strArr;
+
+//   let strCount = {};
+//   strArr.forEach((e) => {
+//     strCount[e] = strCount[e] ? strCount[e] + 1 : 1;
+//   });
+
+//   const sorted = Object.keys(strCount).sort(
+//     (a, b) => strCount[b] - strCount[a]
+//   );
+
+//   return sorted.slice(0, 3);
+// }
+
 function topThreeWords(text) {
-  const strArr = text
-    .split(" ")
-    .map((e) => e.toLowerCase().replace(/[^a-z']|/g, ""))
-    .filter((x) => x !== "'" && x !== "");
+  let words = {};
 
-  if (strArr.length <= 1) return strArr;
+  // Match regex (word that starts with a-z and may have more than 1 a-z)
+  // If matched => count appearances
+  text
+    .toLowerCase()
+    .replace(
+      /([a-z][a-z']*)/g,
+      (match) => (words[match] = words[match] ? words[match]++ : 1)
+    );
 
-  let strCount = {};
-  strArr.forEach((e) => {
-    strCount[e] = strCount[e] ? strCount[e] + 1 : 1;
-  });
-
-  const sorted = Object.keys(strCount).sort(
-    (a, b) => strCount[b] - strCount[a]
-  );
-
-  return sorted.slice(0, 3);
+  return Object.keys(words)
+    .sort((a, b) => words[b] - words[a])
+    .slice(0, 3);
 }
 
 console.log(topThreeWords("a a a  b  c c  d d d d  e e e e e")); // ["e","d","a"]
