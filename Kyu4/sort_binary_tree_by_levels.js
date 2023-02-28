891337b403250dcf13857ab8e2bcac2df4850d92
Wenyu Zheng <zhengwy229@gmail.com>
2023-02-28 15:59:00 +0000

    pass one tests

--- Kyu4/sort_binary_tree_by_levels.js
+++ Kyu4/sort_binary_tree_by_levels.js
@@ -1,8 +1,19 @@
-function treeByLevels(rootNode, res = []) {
+function treeByLevels(rootNode) {
   if (!rootNode) return [];
 
-  res.push(rootNode.value, rootNode.left.value, rootNode.right.value);
-  //   return treeByLevels()
+  let res = [rootNode.value];
+  let nodes = [rootNode.left, rootNode.right];
+
+  while (nodes.length !== 0) {
+    const curr = nodes.shift();
+    res.push(curr.value);
+    if (curr.left) {
+      nodes.push(curr.left);
+    }
+    if (curr.right) {
+      nodes.push(curr.right);
+    }
+  }
 
   return res;
 }
