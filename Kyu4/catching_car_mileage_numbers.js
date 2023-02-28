088bfb956d316f9a8303b1c7e5698cf5537fb343
Wenyu Zheng <zhengwy229@gmail.com>
2023-02-28 01:20:26 +0000

    Pass one more criteria

--- Kyu4/catching_car_mileage_numbers.js
+++ Kyu4/catching_car_mileage_numbers.js
@@ -1,5 +1,10 @@
 function isInteresting(number, awesomePhrases) {
-  // Criteria 1: The digits match one of the values in the awesomePhrases array
+  if (number.toString().length < 3) return 0;
+
+  // Criteria 1: Any digit followed by all zeros
+  if (number.toString().match(/^[0-9]0+$/)) return 2;
+
+  // Criteria 6: The digits match one of the values in the awesomePhrases array
   if (awesomePhrases.includes(number)) return 2;
   for (let i = 0; i < awesomePhrases.length; i++) {
     if (awesomePhrases[i] - 1 === number || awesomePhrases[i] - 2 === number) {
@@ -8,14 +13,16 @@ function isInteresting(number, awesomePhrases) {
     }
   }
 
-  return number.toString().split("");
+  //   return number.toString().split("");
 
-  //   return 0;
+  return 0;
 }
 
-console.log(isInteresting(3, [1337, 256])); // 0
+// console.log(isInteresting(3, [1337, 256])); // 0
 // console.log(isInteresting(1336, [1337, 256])); // 1
 // console.log(isInteresting(1337, [1337, 256])); // 2
+
+console.log(isInteresting(10000, [])); // 2
 console.log(isInteresting(11208, [1337, 256])); // 0
 console.log(isInteresting(11209, [1337, 256])); // 1
 console.log(isInteresting(11211, [1337, 256])); // 2
