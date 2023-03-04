8363d07c7c1058e2e0a2a3f96505109a5fbe312e
Wenyu Zheng <zhengwy229@gmail.com>
2023-03-04 16:48:25 +0000

    New method

--- Kyu4/ten_pin_bowling.js
+++ Kyu4/ten_pin_bowling.js
@@ -1,17 +1,43 @@
 function bowlingScore(frames) {
   const framesArr = frames.split(" ");
 
-  let result = 0;
+  let score = 0;
 
   for (let i = 0; i < framesArr.length; i++) {
-    const frame = framesArr[i].split("");
-    frame.forEach((roll) => {
-      result += parseInt(roll);
-    });
+    // if (framesArr[i] === "X") {
+    //   score += 10 + framesArr[i + 1];
+    // }
+    // if (framesArr[i].includes("/")) {
+    //   score += 10 + parseInt(framesArr[i + 1][0]);
+    // }
+
+    score += getFrameScore(framesArr[i]);
+
+    // const frame = framesArr[i].split("");
+    // let extraScore = 0;
+    // for (let j = 0; j < frame.length; j++) {
+    //   let result = 0;
+    //   if (frame[j] === "X") {
+    //     result = 10;
+    //     extraScore += 2;
+    //   } else {
+    //     result = parseInt(frame[j]);
+    //   }
+    //   if (extraScore !== 0) {
+    //     result += result;
+    //     extraScore--;
+    //   }
+    //   console.log({ i, j, extraScore, result });
+    //   score += result;
+    // }
   }
 
-  return result;
+  return score;
 }
+
+const getFrameScore = (frame) => {
+  return frame.split("").reduce((sum, roll) => (sum += parseInt(roll)), 0);
+};
 
 console.log(bowlingScore("11 11 11 11 11 11 11 11 11 11")); // 20
 // console.log(bowlingScore("X X X X X X X X X XXX")); // 300
