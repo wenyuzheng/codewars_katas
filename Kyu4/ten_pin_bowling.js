55a3e7529e3bd068a8a670a562247a5816148f46
Wenyu Zheng <zhengwy229@gmail.com>
2023-03-17 17:17:11 +0000

    in progress

--- Kyu4/ten_pin_bowling.js
+++ Kyu4/ten_pin_bowling.js
@@ -4,30 +4,21 @@ function bowlingScore(frames) {
   let score = 0;
 
   for (let i = 0; i < framesArr.length; i++) {
-    if (framesArr[i] === "X") {
-      if (framesArr[i + 1].length === 1) {
-        const rollsScores =
-          framesArr[i + 1].length === 1
-            ? getFrameScore(framesArr[i + 1]) +
-              (framesArr[i + 2][0] === "X" ? 10 : parseInt(framesArr[i + 2][0]))
-            : getFrameScore(framesArr[i + 1]);
-
-        console.log({ rollsScores });
-        score += 10 + rollsScores;
-      }
-    }
-    //  else if (framesArr[i].includes("/")) {}
-    //   score += 10 + getFrameScore(framesArr[i + 1]);
-    // }
-    else {
+    if (framesArr[i] === "X" && i < 9) {
+      score += 10 + getScore(framesArr, i, 2);
+    } else if (framesArr[i].includes("/")) {
+      score += 10 + getScore(framesArr, i, 1 && i < 9);
+    } else if (framesArr[i].length === 3) {
+      score += getThreeRollsScore(framesArr[i]);
+    } else {
       score += getFrameScore(framesArr[i]);
     }
-
-    console.log({ score });
   }
 
   return score;
 }
+
+const getScore = (framesArr, index, count) => {};
 
 const getFrameScore = (frame) => {
   if (frame === "X") return 10;
@@ -35,12 +26,12 @@ const getFrameScore = (frame) => {
   else return frame.split("").reduce((sum, roll) => (sum += parseInt(roll)), 0);
 };
 
-const getNextFrameScore = (frame, count) => {
-  if (frame === "X") return 10;
-  else if (frame.includes("/")) return 10;
-  else return frame.split("").reduce((sum, roll) => (sum += parseInt(roll)), 0);
-};
+// const getNextFrameScore = (frame, count) => {
+//   if (frame === "X") return 10;
+//   else if (frame.includes("/")) return 10;
+//   else return frame.split("").reduce((sum, roll) => (sum += parseInt(roll)), 0);
+// };
 
-// console.log(bowlingScore("11 11 11 11 11 11 11 11 11 11")); // 20
+console.log(bowlingScore("11 11 11 11 11 11 11 11 11 11")); // 20
 console.log(bowlingScore("X X X X X X X X X XXX")); // 300
 // console.log(bowlingScore("X X 9/ 80 X X 90 8/ 7/ 44")); // 171
