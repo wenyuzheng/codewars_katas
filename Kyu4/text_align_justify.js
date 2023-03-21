f8333dbfcb95a7cfa8c8470173c6049a34f6f672
Wenyu Zheng <zhengwy229@gmail.com>
2023-03-21 23:20:16 +0000

    in progress

--- Kyu4/text_align_justify.js
+++ Kyu4/text_align_justify.js
@@ -1,10 +1,30 @@
 export function justify(text, width) {
-  let texts = [];
-  for (let i = 0; i < Math.floor(text.length / width); i++) {
-    texts.push(text.slice(i * width, width));
-  }
-  texts.push(text.slice(Math.floor(text.length / width) * width, text.length));
-  console.log({ texts });
+  const words = text.split(" ");
 
-  return texts.join("\n");
+  let textArr = [];
+
+  let i = 0;
+
+  while (words.length !== 0) {
+    //   for (let i = 0; i < words.length; i++) {
+    let tempWidth = width;
+    while (tempWidth - words[i].length >= 0) {
+      tempWidth -= words[i].length;
+      textArr.push(words[i]);
+      words.shift();
+      i++;
+    }
+    textArr.push("\n");
+  }
+
+  console.log({ textArr });
+
+  //   let texts = [];
+  //   for (let i = 0; i < Math.floor(text.length / width); i++) {
+  //     texts.push(text.slice(i * width, width));
+  //   }
+  //   texts.push(text.slice(Math.floor(text.length / width) * width, text.length));
+  //   console.log({ texts });
+
+  //   return texts.join("\n");
 }
