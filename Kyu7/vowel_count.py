caef069a9aaf0c1bf32a44ccf0fce47218f26c38
Wenyu Zheng <zhengwy229@gmail.com>
2023-03-23 23:34:54 +0000

    refactore

--- Kyu7/vowel_count.py
+++ Kyu7/vowel_count.py
@@ -1,15 +1,9 @@
 def get_count(sentence):
-    if len(sentence) == 0: return 0
-
-    vowels = "aeiou"
     count = 0
-
     for char in sentence:
-        if char in vowels: 
+        if char in "aeiou": 
             count += 1
-
     return count
-
 
 print(get_count("aeiou")) # 5
 print(get_count("bcdfghjklmnpqrstvwxz y")) # 0
