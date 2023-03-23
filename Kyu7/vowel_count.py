df801f304428b3289b72ebe2f0bc1731c609beb9
Wenyu Zheng <zhengwy229@gmail.com>
2023-03-23 23:36:06 +0000

    Improve version

--- Kyu7/vowel_count.py
+++ Kyu7/vowel_count.py
@@ -1,9 +1,12 @@
+# def get_count(sentence):
+#     count = 0
+#     for char in sentence:
+#         if char in "aeiou": 
+#             count += 1
+#     return count
+
 def get_count(sentence):
-    count = 0
-    for char in sentence:
-        if char in "aeiou": 
-            count += 1
-    return count
+    return sum(char in "aeiou" for char in sentence)
 
 print(get_count("aeiou")) # 5
 print(get_count("bcdfghjklmnpqrstvwxz y")) # 0
