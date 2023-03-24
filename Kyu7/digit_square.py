c0d4ec3dcf6c0b77f862eedfea1734e61a0f4738
Wenyu Zheng <zhengwy229@gmail.com>
2023-03-24 00:25:41 +0000

    one line version

--- Kyu7/digit_square.py
+++ Kyu7/digit_square.py
@@ -1,8 +1,11 @@
+# def square_digits(num):
+#     newNum = ""
+#     for digit in str(num):
+#         newNum += str(int(digit) ** 2)
+#     return int(newNum)
+
 def square_digits(num):
-    newNum = ""
-    for digit in str(num):
-        newNum += str(int(digit) ** 2)
-    return int(newNum)
+    return int("".join(str(int(digit) ** 2) for digit in str(num)))
 
 print(square_digits(9119)) # 811181
 print(square_digits(0)) # 0
