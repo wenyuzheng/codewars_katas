5a2b7f48c3e3c2b2b7f57b01c7bf6a8164d6181d
Wenyu Zheng <zhengwy229@gmail.com>
2023-03-24 00:01:04 +0000

    One line version

--- Kyu7/disemvowel.py
+++ Kyu7/disemvowel.py
@@ -1,8 +1,11 @@
+# def disemvowel(string_):
+#   newString = ""
+#   for char in string_:
+#     if char.lower() not in "aeiou":
+#       newString += char
+#   return newString
+
 def disemvowel(string_):
-  newString = ""
-  for char in string_:
-    if char.lower() not in "aeiou":
-      newString += char
-  return newString
+  return "".join(char for char in string_ if char.lower() not in "aeiou")
 
 print(disemvowel("This website is for losers LOL!")) # "Ths wbst s fr lsrs LL!"
