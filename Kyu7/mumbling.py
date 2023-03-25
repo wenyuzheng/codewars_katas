f1d95adf716410eb9ce749eeb535d49c2957361c
Wenyu Zheng <zhengwy229@gmail.com>
2023-03-25 01:07:21 +0000

    one line version

--- Kyu7/mumbling.py
+++ Kyu7/mumbling.py
@@ -1,11 +1,17 @@
+# def accum(s):
+#     string = []
+#     count = 1
+#     for c in s:
+#         string.append((c * count).capitalize())
+#         count += 1
+
+#     return ("-").join(string)
+
 def accum(s):
-    string = []
-    count = 1
-    for c in s:
-        string.append((c * count).capitalize())
-        count += 1
+    return ("-").join((c * count).capitalize() for count, c in enumerate(s, 1))
 
-    return ("-").join(string)
-
+# enumerate() returns (index, item)
+# e.g. mylist = ['A', 'B' ,'C', 'D'] => enumerate(mylist, 2) = [(2, 'A'), (3, 'B'), (4, 'C'), (5, 'D')]
+    
 print(accum("ZpglnRxqenU")) # "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
 print(accum("abcd")) # "A-Bb-Ccc-Dddd"
