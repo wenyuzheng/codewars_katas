2025ff549b9df4042db0b8fcb62ed57029143af9
Wenyu Zheng <zhengwy229@gmail.com>
2023-03-27 08:51:16 +0100

    in progress

--- Kyu7/is_square.py
+++ Kyu7/is_square.py
@@ -3,6 +3,8 @@ import math
 def is_square(n):    
   return n >= 0 and math.sqrt(n) ** 2 == n
   
+def test():
+  assert 1+1 == 2
 
 print(is_square(-1)) # False
 print(is_square(0)) # True
--- algorithms/dijkstra.js
+++ algorithms/dijkstra.js
@@ -1,14 +1,22 @@
-class Node {
-  constructor(root, children) {
-    this.root = root;
-    this.children = children;
-  }
-}
+// class Node {
+//   constructor(root, edges) {
+//     this.root = root;
+//     this.edges = edges;
+//   }
+// }
 
-class Vertex {
-  constructor(weight, node1, node2) {
-    this.weight = weight;
-    this.node1 = node1;
-    this.node2 = node2;
-  }
-}
+// class Edge {
+//   constructor(weight, node1, node2) {
+//     this.weight = weight;
+//     this.node1 = node1;
+//     this.node2 = node2;
+//   }
+// }
+
+graph = {
+  a: ["b", "c"],
+  b: ["a"],
+  c: ["a", "d"],
+  d: ["c", "e"],
+  e: ["d"],
+};
