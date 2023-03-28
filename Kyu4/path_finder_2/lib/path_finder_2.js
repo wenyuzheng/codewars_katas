5d90e2e440744f8c17dc78b4ae57a61101b080bb
Wenyu Zheng <zhengwy229@gmail.com>
2023-03-28 12:31:13 +0100

    pass tests

--- Kyu4/path_finder_2/lib/path_finder_2.js
+++ Kyu4/path_finder_2/lib/path_finder_2.js
@@ -1,15 +1,31 @@
 const { dijkstraAll } = require("./dijkstraAll");
-const { getGraph } = require("./getGraph");
+// const { getGraph } = require("./getGraph");
 
+// const path_finder = (maze) => {
+//   const mazeArr = maze.split("\n").map((e) => e.split(""));
+//   const n = mazeArr.length - 1;
+//   const goalNode = `${n},${n}`;
+
+//   let graph = getGraph(maze);
+//   graph = dijkstraAll(graph, "0,0");
+
+//   if (!graph[goalNode].finalVal) return false;
+//   return graph[goalNode].finalVal;
+// };
+
+const { getGraph } = require("./getGraph_v2");
+
+// With getGraph version 2
 const path_finder = (maze) => {
   const mazeArr = maze.split("\n").map((e) => e.split(""));
   const n = mazeArr.length - 1;
+  const startNode = "0,0";
   const goalNode = `${n},${n}`;
 
-  let graph = getGraph(maze);
-  graph = dijkstraAll(graph, "0,0");
+  let graph = getGraph(maze, startNode);
+  graph = dijkstraAll(graph, startNode);
 
-  if (!graph[goalNode].finalVal) return false;
+  if (!graph[goalNode]) return false;
   return graph[goalNode].finalVal;
 };
 
--- Kyu4/path_finder_2/test/getGraph_v2.test.js
+++ Kyu4/path_finder_2/test/getGraph_v2.test.js
@@ -18,4 +18,13 @@ describe("get graph v2", () => {
     expect(result["0,2"]).toEqual(undefined);
     expect(result["1,2"]).toEqual(undefined);
   });
+
+  it("eg 3", () => {
+    const maze = ".W.\n.W.";
+    const result = getGraph(maze, "0,0");
+    expect(result["0,0"].neighbours).toEqual(["1,0"]);
+    expect(result["1,0"].neighbours).toEqual(["0,0"]);
+    expect(result["0,2"]).toEqual(undefined);
+    expect(result["1,2"]).toEqual(undefined);
+  });
 });
