f6a583909c58a7edf00381d8694fee2482c086b1
Wenyu Zheng <zhengwy229@gmail.com>
2023-11-06 15:07:32 +0000

    Improved but not working

--- Kyu5/did_you_mean.py
+++ Kyu5/did_you_mean.py
@@ -9,23 +9,28 @@ class Dictionary:
         for word in self.words:
             total = 0
             for i in range(len(term)):
-                for j in range(i, len(word)):
-                    # print(word, i, term[i], j, word[j])
+                matched = False
+                for j in range(len(word)):
+                    matched = False
                     if term[i] == word[j]:
+                        matched = True
                         break
-                    else:
-                        total+=1
+                if matched:
+                    continue
+                else:
+                    print(word, i, term[i], j, word[j])
+                    total+=1
+
             diff.append(total)
             print(diff, diff.index(min(diff)))
 
-        
         return self.words[diff.index(min(diff))]
 
 words = ['cherry', 'peach', 'pineapple', 'melon', 'strawberry', 'raspberry', 'apple', 'coconut', 'banana']
 test_dict = Dictionary(words)
 print(test_dict.find_most_similar('strawbery')) # 'strawberry'
 print(test_dict.find_most_similar('berry')) # 'cherry'
-print(test_dict.find_most_similar('aple'))  # 'apple'
+# print(test_dict.find_most_similar('aple'))  # 'apple'
 
-things = Dictionary(['stars', 'mars', 'wars', 'codec', 'codewars'])
-print(things.find_most_similar('coddwars')) # "codewars"
+# things = Dictionary(['stars', 'mars', 'wars', 'codec', 'codewars'])
+# print(things.find_most_similar('coddwars')) # "codewars"
