f6d716d04e65771235ae4b38edad040cea82af57
Wenyu Zheng <zhengwy229@gmail.com>
2023-11-07 22:25:41 +0000

    Pass more tests

--- Kyu5/did_you_mean.py
+++ Kyu5/did_you_mean.py
@@ -1,65 +1,89 @@
+# class Dictionary:
+#     def __init__(self, words):
+#         self.words = words
+#     # def find_most_similar(self, term):
+#     #     if term in self.words:
+#     #         return term
+        
+#     #     diff = []
+#     #     for word in self.words:
+#     #         total = 0
+#     #         for i in range(len(term)):
+#     #             matched = False
+#     #             for j in range(len(word)):
+#     #                 matched = False
+#     #                 if term[i] == word[j]:
+#     #                     matched = True
+#     #                     break
+#     #             if matched:
+#     #                 continue
+#     #             else:
+#     #                 print(word, i, term[i], j, word[j])
+#     #                 total+=1
+
+#     #         diff.append(total)
+#     #         print(diff, diff.index(min(diff)))
+
+#     #     return self.words[diff.index(min(diff))]
+
+#     def find_most_similar(self, term):
+#         if term in self.words:
+#             return term
+        
+#         diff = []
+#         for word in self.words:
+#             total = 0
+#             for i in range(len(term)):
+#                 matched = False
+#                 index = 0
+#                 for j in range(index, len(word)):
+#                     matched = False
+#                     if term[i] == word[j]:
+#                         print(word, i, term[i], j, word[j])
+
+#                         matched = True
+#                         index = j + 1
+#                         break
+#                 if matched:
+#                     continue
+#                 else:
+#                     # print(word, i, term[i], j, word[j])
+#                     total+=1
+
+#             diff.append(total)
+#             print(diff, diff.index(min(diff)))
+
+#         return self.words[diff.index(min(diff))]
+
 class Dictionary:
     def __init__(self, words):
         self.words = words
-    # def find_most_similar(self, term):
-    #     if term in self.words:
-    #         return term
-        
-    #     diff = []
-    #     for word in self.words:
-    #         total = 0
-    #         for i in range(len(term)):
-    #             matched = False
-    #             for j in range(len(word)):
-    #                 matched = False
-    #                 if term[i] == word[j]:
-    #                     matched = True
-    #                     break
-    #             if matched:
-    #                 continue
-    #             else:
-    #                 print(word, i, term[i], j, word[j])
-    #                 total+=1
-
-    #         diff.append(total)
-    #         print(diff, diff.index(min(diff)))
-
-    #     return self.words[diff.index(min(diff))]
-
     def find_most_similar(self, term):
-        if term in self.words:
-            return term
-        
         diff = []
         for word in self.words:
             total = 0
-            for i in range(len(term)):
-                matched = False
-                index = 0
-                for j in range(index, len(word)):
-                    matched = False
-                    if term[i] == word[j]:
-                        print(word, i, term[i], j, word[j])
-
-                        matched = True
-                        index = j + 1
-                        break
-                if matched:
-                    continue
-                else:
-                    # print(word, i, term[i], j, word[j])
-                    total+=1
+            for t in term:
+                if t not in word: 
+                    total += 1
+            
+            for w in word:
+                if w not in term: 
+                    total += 1
 
             diff.append(total)
-            print(diff, diff.index(min(diff)))
 
+        print(diff)
         return self.words[diff.index(min(diff))]
 
+    
 words = ['cherry', 'peach', 'pineapple', 'melon', 'strawberry', 'raspberry', 'apple', 'coconut', 'banana']
 test_dict = Dictionary(words)
 print(test_dict.find_most_similar('strawbery')) # 'strawberry'
-# print(test_dict.find_most_similar('berry')) # 'cherry'
-# print(test_dict.find_most_similar('aple'))  # 'apple'
+print(test_dict.find_most_similar('berry')) # 'cherry'
+print(test_dict.find_most_similar('aple'))  # 'apple'
 
-# things = Dictionary(['stars', 'mars', 'wars', 'codec', 'codewars'])
-# print(things.find_most_similar('coddwars')) # "codewars"
+things = Dictionary(['stars', 'mars', 'wars', 'codec', 'codewars'])
+print(things.find_most_similar('coddwars')) # "codewars"
+
+languages = Dictionary(['brainfuck', 'cpp', 'php', 'java', 'javascript', 'python', 'ruby', 'coffeescript', 'c'])
+print(languages.find_most_similar('heaven')) # "java"
