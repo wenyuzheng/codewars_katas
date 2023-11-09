058aead6368bb85a2e9a9500646a992eb82f6e71
Wenyu Zheng <zhengwy229@gmail.com>
2023-11-09 21:49:26 +0000

    Changes on count

--- Kyu5/text_align_justify.py
+++ Kyu5/text_align_justify.py
@@ -6,13 +6,13 @@ def justify(text, width):
     lineLen = 0
 
     for w in words:
-        count += len(w) + 1 if w != words[-1] else len(w)
+        count += len(w)
 
         if count <= width:
             lineArr.append(w)
             lineLen += len(w)
         else: 
-            count = len(w) + 1
+            count = len(w)
             lines.append({"words": lineArr, "length": lineLen})
             lineArr = [w]
             lineLen = len(w)
@@ -20,6 +20,7 @@ def justify(text, width):
         if w == words[-1]: 
             lines.append({"words": lineArr, "length": lineLen})
         
+        count += 1
 
     result = ""
     for line in lines:
@@ -44,3 +45,5 @@ def justify(text, width):
     return result
 
 print(justify('123 45 6', 7)) # '123  45\n6'
+# print(justify("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis dolor mauris, at elementum ligula tempor eget. In quis rhoncus nunc, at aliquet orci. Fusce at dolor sit amet felis suscipit tristique. Nam a imperdiet tellus. Nulla eu vestibulum urna. Vivamus tincidunt suscipit enim, nec ultrices nisi volutpat ac. Maecenas sit amet lacinia arcu, non dictum justo. Donec sed quam vel risus faucibus euismod. Suspendisse rhoncus rhoncus felis at fermentum. Donec lorem magna, ultricies a nunc sit amet, blandit fringilla nunc. In vestibulum velit ac felis rhoncus pellentesque. Mauris at tellus enim. Aliquam eleifend tempus dapibus. Pellentesque commodo, nisi sit amet hendrerit fringilla, ante odio porta lacus, ut elementum justo nulla et dolor.", 15))
+# print(justify("Lorem ipsum dolor sit amet, consectetur", 15))
