52a5e6f3eb39dacbc6b4ae277c63a952045f59c5
Wenyu Zheng <zhengwy229@gmail.com>
2023-11-09 21:57:49 +0000

    Add space between words on last line

--- Kyu5/text_align_justify.py
+++ Kyu5/text_align_justify.py
@@ -27,7 +27,7 @@ def justify(text, width):
         words = line["words"]
 
         if line == lines[-1]:
-            result += "".join(words)
+            result += " ".join(words)
             break
 
         spacesNum = width - line["length"]
@@ -42,8 +42,10 @@ def justify(text, width):
 
         result += "".join(words) + "\n"
 
-    return result
+    return result.strip()
 
-print(justify('123 45 6', 7)) # '123  45\n6'
-# print(justify("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis dolor mauris, at elementum ligula tempor eget. In quis rhoncus nunc, at aliquet orci. Fusce at dolor sit amet felis suscipit tristique. Nam a imperdiet tellus. Nulla eu vestibulum urna. Vivamus tincidunt suscipit enim, nec ultrices nisi volutpat ac. Maecenas sit amet lacinia arcu, non dictum justo. Donec sed quam vel risus faucibus euismod. Suspendisse rhoncus rhoncus felis at fermentum. Donec lorem magna, ultricies a nunc sit amet, blandit fringilla nunc. In vestibulum velit ac felis rhoncus pellentesque. Mauris at tellus enim. Aliquam eleifend tempus dapibus. Pellentesque commodo, nisi sit amet hendrerit fringilla, ante odio porta lacus, ut elementum justo nulla et dolor.", 15))
+# print(justify('123 45 6', 7)) # '123  45\n6'
+
+text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis dolor mauris, at elementum ligula tempor eget. In quis rhoncus nunc, at aliquet orci. Fusce at dolor sit amet felis suscipit tristique. Nam a imperdiet tellus. Nulla eu vestibulum urna. Vivamus tincidunt suscipit enim, nec ultrices nisi volutpat ac. Maecenas sit amet lacinia arcu, non dictum justo. Donec sed quam vel risus faucibus euismod. Suspendisse rhoncus rhoncus felis at fermentum. Donec lorem magna, ultricies a nunc sit amet, blandit fringilla nunc. In vestibulum velit ac felis rhoncus pellentesque. Mauris at tellus enim. Aliquam eleifend tempus dapibus. Pellentesque commodo, nisi sit amet hendrerit fringilla, ante odio porta lacus, ut elementum justo nulla et dolor."
+print(justify(text, 30))
 # print(justify("Lorem ipsum dolor sit amet, consectetur", 15))
