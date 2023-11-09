4de6e3994691c4e6ad5932602c0977c3d8e1d085
Wenyu Zheng <zhengwy229@gmail.com>
2023-11-09 21:58:50 +0000

    Remove start and end spaces

--- Kyu5/text_align_justify.py
+++ Kyu5/text_align_justify.py
@@ -40,12 +40,12 @@ def justify(text, width):
             spacesNum -= 1
             i += 1
 
-        result += "".join(words) + "\n"
+        result += "".join(words).strip() + "\n"
 
     return result.strip()
 
 # print(justify('123 45 6', 7)) # '123  45\n6'
 
 text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis dolor mauris, at elementum ligula tempor eget. In quis rhoncus nunc, at aliquet orci. Fusce at dolor sit amet felis suscipit tristique. Nam a imperdiet tellus. Nulla eu vestibulum urna. Vivamus tincidunt suscipit enim, nec ultrices nisi volutpat ac. Maecenas sit amet lacinia arcu, non dictum justo. Donec sed quam vel risus faucibus euismod. Suspendisse rhoncus rhoncus felis at fermentum. Donec lorem magna, ultricies a nunc sit amet, blandit fringilla nunc. In vestibulum velit ac felis rhoncus pellentesque. Mauris at tellus enim. Aliquam eleifend tempus dapibus. Pellentesque commodo, nisi sit amet hendrerit fringilla, ante odio porta lacus, ut elementum justo nulla et dolor."
-print(justify(text, 30))
+print(justify(text, 20))
 # print(justify("Lorem ipsum dolor sit amet, consectetur", 15))
